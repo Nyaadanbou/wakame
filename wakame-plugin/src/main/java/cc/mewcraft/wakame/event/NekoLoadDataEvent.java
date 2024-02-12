978846c73bd91cd0564d1d8bf2c46bc097e2be60
Akiranya <porridgen@gmail.com>
2024-02-13 00:54:23 +0800

    fix java class not included in the final jar

--- wakame-plugin/src/main/java/cc/mewcraft/wakame/event/NekoLoadDataEvent.java
+++ wakame-plugin/src/main/java/cc/mewcraft/wakame/event/NekoLoadDataEvent.java
@@ -5,20 +5,20 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 public class NekoLoadDataEvent extends Event {
-    
+
     private static final @NotNull HandlerList HANDLERS = new HandlerList();
-    
+
     public NekoLoadDataEvent() {
         super(true);
     }
-    
+
     public static @NotNull HandlerList getHandlerList() {
         return HANDLERS;
     }
-    
+
     @Override
     public @NotNull HandlerList getHandlers() {
         return HANDLERS;
     }
-    
+
 }
