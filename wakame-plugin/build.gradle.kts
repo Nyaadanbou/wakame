0d329ff245417f1ae5dc52039f5edb62eba9b971
Akiranya <porridgen@gmail.com>
2024-04-22 18:02:49 +0800

    shift release version to SNAPSHOT version

--- wakame-plugin/build.gradle.kts
+++ wakame-plugin/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 project.ext.set("name", "Wakame")
 
 group = "cc.mewcraft.wakame"
-version = "1.0.0"
+version = "1.0.0-SNAPSHOT"
 description = "Add custom stuff to server"
 
 dependencies {
