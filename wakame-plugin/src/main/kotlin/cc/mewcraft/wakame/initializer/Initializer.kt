f3fa2de4c01b9bdad921dc0c2594e2d460ba0bc8
Akiranya <porridgen@gmail.com>
2024-04-25 19:46:03 +0800

    clean up TestListener

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/initializer/Initializer.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/initializer/Initializer.kt
@@ -20,7 +20,6 @@ import cc.mewcraft.wakame.pack.PackException
 import cc.mewcraft.wakame.pack.ResourcePackListener
 import cc.mewcraft.wakame.pack.ResourcePackManager
 import cc.mewcraft.wakame.registry.*
-import cc.mewcraft.wakame.test.TestListener
 import cc.mewcraft.wakame.user.PaperUserManager
 import cc.mewcraft.wakame.util.registerEvents
 import cc.mewcraft.wakame.util.unregisterEvents
@@ -124,7 +123,6 @@ object Initializer : KoinComponent, Listener {
         registerTerminableListener(get<PaperUserManager>()).bindWith(this)
         registerTerminableListener(get<ResourcePackListener>()).bindWith(this)
         registerTerminableListener(get<SingleItemListener>()).bindWith(this)
-        registerTerminableListener(get<TestListener>()).bindWith(this)
     }
 
     private fun registerCommands() {
