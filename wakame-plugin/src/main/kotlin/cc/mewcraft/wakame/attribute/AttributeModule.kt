8b0843249ea4376435c8202860ec39404b992baf
Akiranya <porridgen@gmail.com>
2024-05-17 21:09:50 +0800

    fix DefaultAttributes not being registered into Initializer (???)
    It's so weird that `single<Initializable> { ... }` can't properly register the definition as a type of `Initializable`

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attribute/AttributeModule.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attribute/AttributeModule.kt
@@ -3,9 +3,10 @@ package cc.mewcraft.wakame.attribute
 import cc.mewcraft.wakame.initializer.Initializable
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal fun attributeModule(): Module = module {
     singleOf(::AttributeEventHandler)
-    single<Initializable> { DefaultAttributes }
+    single { DefaultAttributes } bind Initializable::class
 }
