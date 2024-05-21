3d6e1f3f8696fefd0dce249fb34a03059163206c
Akiranya <porridgen@gmail.com>
2024-05-21 15:58:29 +0800

    interface Cell is now pure marker

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/Cell.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/Cell.kt
@@ -3,11 +3,4 @@ package cc.mewcraft.wakame.item
 /**
  * A [cell][Cell] is conceptually a container which contains zero or more [cores][Core].
  */
-interface Cell {
-    // /**
-    //  * The ID of this cell.
-    //  *
-    //  * No cells with identical ID on a single item.
-    //  */
-    // val id: String // FIXME cell-overhaul: 移除?
-}
+interface Cell
