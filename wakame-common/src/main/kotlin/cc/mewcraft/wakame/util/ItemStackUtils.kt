6f4aa0415b511c679f7d926ea178cf73499066bf
Akiranya <porridgen@gmail.com>
2024-06-24 19:05:37 +0800

    add extension function: ItemStack#editMeta

--- wakame-common/src/main/kotlin/cc/mewcraft/wakame/util/ItemStackUtils.kt
+++ wakame-common/src/main/kotlin/cc/mewcraft/wakame/util/ItemStackUtils.kt
@@ -1,9 +1,14 @@
 package cc.mewcraft.wakame.util
 
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
 fun ItemStack.takeUnlessEmpty(): ItemStack? =
     if (type.isAir || amount <= 0) null else this
 
 fun ItemStack?.isEmpty(): Boolean =
     this == null || type.isAir || amount <= 0
+
+inline fun <reified T : ItemMeta> ItemStack.editMeta(crossinline block: T.() -> Unit) {
+    this.editMeta(T::class.java) { block(it) }
+}
