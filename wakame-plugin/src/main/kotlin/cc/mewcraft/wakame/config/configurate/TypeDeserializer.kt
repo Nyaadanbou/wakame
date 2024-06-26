f7d9dfdc7d4a66b535babb0a425fb1117c25280a
Akiranya <porridgen@gmail.com>
2024-06-26 23:13:56 +0800

    add override for function: TypeDeserializer#emptyValue

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/config/configurate/TypeDeserializer.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/config/configurate/TypeDeserializer.kt
@@ -1,6 +1,7 @@
 package cc.mewcraft.wakame.config.configurate
 
 import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.ConfigurationOptions
 import org.spongepowered.configurate.serialize.TypeSerializer
 import java.lang.reflect.Type
 
@@ -9,5 +10,7 @@ interface TypeDeserializer<T> : TypeSerializer<T> {
     override fun deserialize(type: Type, node: ConfigurationNode): T
 
     override fun serialize(type: Type, obj: T?, node: ConfigurationNode): Unit = throw UnsupportedOperationException()
+
+    override fun emptyValue(specificType: Type, options: ConfigurationOptions): T? = null
 
 }
