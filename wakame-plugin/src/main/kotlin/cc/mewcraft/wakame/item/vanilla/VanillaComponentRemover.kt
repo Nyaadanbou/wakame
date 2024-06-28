04e248e928d5bf1cd9f287ffe2c1740e608f3679
Akiranya <porridgen@gmail.com>
2024-06-28 17:56:43 +0800

    support null `remove_components` in config

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/vanilla/VanillaComponentRemover.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/vanilla/VanillaComponentRemover.kt
@@ -1,10 +1,10 @@
 package cc.mewcraft.wakame.item.vanilla
 
 import cc.mewcraft.wakame.config.configurate.TypeDeserializer
-import com.google.common.collect.ImmutableMultimap
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.ConfigurationOptions
 import org.spongepowered.configurate.kotlin.extensions.getList
 import java.lang.reflect.Type
 
@@ -16,6 +16,17 @@ interface VanillaComponentRemover {
      * 从指定的物品上移除既定的组件.
      */
     fun applyToItem(item: ItemStack)
+
+    companion object {
+        /**
+         * 获取一个空的实例. 该实例不会进行任何操作.
+         */
+        fun empty(): VanillaComponentRemover = Empty
+    }
+
+    private object Empty : VanillaComponentRemover {
+        override fun applyToItem(item: ItemStack) = Unit
+    }
 }
 
 /**
@@ -23,14 +34,26 @@ interface VanillaComponentRemover {
  */
 internal object VanillaComponentRemoverSerializer : TypeDeserializer<VanillaComponentRemover> {
     override fun deserialize(type: Type, node: ConfigurationNode): VanillaComponentRemover {
-        val toRemove = node.getList<VanillaComponentRemoverImpl.Supported>(emptyList())
-        return VanillaComponentRemoverImpl(toRemove.toSet())
+        val removes = node.getList<NaiveVanillaComponentRemover.Supported>(emptyList()).toSet()
+        return NaiveVanillaComponentRemover(removes)
+    }
+
+    override fun emptyValue(specificType: Type, options: ConfigurationOptions): VanillaComponentRemover? {
+        return EmptyVanillaComponentRemover
     }
 }
 
-// 临时的不完整实现, 仅支持少数几个组件的移除
-// 等 DataComponent API 出来后再写个完整的实现
-private class VanillaComponentRemoverImpl(
+/**
+ * 一个空的实现. 该实现不会对物品进行任何操作.
+ */
+private object EmptyVanillaComponentRemover : VanillaComponentRemover {
+    override fun applyToItem(item: ItemStack) = Unit
+}
+
+/**
+ * 临时的不完整实现, 仅支持少数几个组件的移除.
+ */
+private class NaiveVanillaComponentRemover(
     private val removes: Set<Supported>,
 ) : VanillaComponentRemover {
     private fun applyToMeta(meta: ItemMeta) {
@@ -46,7 +69,7 @@ private class VanillaComponentRemoverImpl(
     enum class Supported {
         ATTRIBUTE_MODIFIERS {
             override fun applyToMeta(meta: ItemMeta) {
-                meta.attributeModifiers = ImmutableMultimap.of() // bad implementation
+                meta.attributeModifiers = null
             }
         },
         FOOD {
