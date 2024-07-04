4b9d762385b9c1f6b761147c668058ba3fa30239
Akiranya <porridgen@gmail.com>
2024-07-04 23:00:11 +0800

    hand over code to g22

--- wakame-plugin/src/test/kotlin/ItemModelDataLookupTest.kt
+++ wakame-plugin/src/test/kotlin/ItemModelDataLookupTest.kt
@@ -1,23 +1,7 @@
-import cc.mewcraft.wakame.adventure.adventureModule
-import cc.mewcraft.wakame.element.elementModule
-import cc.mewcraft.wakame.entity.entityModule
-import cc.mewcraft.wakame.item.itemModule
-import cc.mewcraft.wakame.kizami.kizamiModule
 import cc.mewcraft.wakame.lookup.ItemModelDataLookup
 import cc.mewcraft.wakame.lookup.lookupModule
-import cc.mewcraft.wakame.rarity.rarityModule
-import cc.mewcraft.wakame.registry.AttributeRegistry
-import cc.mewcraft.wakame.registry.ElementRegistry
-import cc.mewcraft.wakame.registry.EntityRegistry
 import cc.mewcraft.wakame.registry.ItemRegistry
 import cc.mewcraft.wakame.registry.ItemRegistry.get
-import cc.mewcraft.wakame.registry.KizamiRegistry
-import cc.mewcraft.wakame.registry.LevelMappingRegistry
-import cc.mewcraft.wakame.registry.RarityRegistry
-import cc.mewcraft.wakame.registry.SkillRegistry
-import cc.mewcraft.wakame.registry.registryModule
-import cc.mewcraft.wakame.skill.skillModule
-import cc.mewcraft.wakame.skin.skinModule
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -39,31 +23,14 @@ class ItemModelDataLookupTest : KoinTest {
 
                 // this module
                 modules(
-                    itemModule()
+                    // itemModule()
                 )
 
                 // dependencies
                 modules(
-                    adventureModule(),
-                    elementModule(),
-                    entityModule(),
-                    kizamiModule(),
-                    registryModule(),
-                    rarityModule(),
-                    skillModule(),
-                    skinModule(),
                     lookupModule()
                 )
             }
-
-            AttributeRegistry.onPreWorld()
-            ElementRegistry.onPreWorld()
-            KizamiRegistry.onPreWorld()
-            RarityRegistry.onPreWorld()
-            SkillRegistry.onPreWorld()
-            LevelMappingRegistry.onPreWorld()
-            EntityRegistry.onPreWorld()
-            ItemRegistry.onPreWorld()
         }
 
         @JvmStatic
