7e56c38059d3f11a793c0fe78e5c2d5501e4ab02
Akiranya <porridgen@gmail.com>
2024-07-05 21:41:23 +0800

    correct return type of ItemComponentBridge.templateType()

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/components/CustomModelData.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/components/CustomModelData.kt
@@ -4,7 +4,6 @@ import cc.mewcraft.wakame.item.component.ItemComponent
 import cc.mewcraft.wakame.item.component.ItemComponentBridge
 import cc.mewcraft.wakame.item.component.ItemComponentHolder
 import cc.mewcraft.wakame.item.component.ItemComponentType
-import cc.mewcraft.wakame.item.template.ItemTemplate
 import cc.mewcraft.wakame.item.template.ItemTemplateType
 import cc.mewcraft.wakame.util.backingCustomModelData
 import net.kyori.examination.Examinable
@@ -22,7 +21,7 @@ data class CustomModelData(
         // 设计上 CustomModelData 不能由物品的配置文件决定,
         // 因此这里也没写对应的 Template 实现
 
-        override fun templateType(): ItemTemplateType<ItemTemplate<CustomModelData>> {
+        override fun templateType(): ItemTemplateType<Nothing> {
             throw UnsupportedOperationException()
         }
     }
