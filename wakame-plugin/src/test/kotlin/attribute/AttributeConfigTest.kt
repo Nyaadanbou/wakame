490b570490e7075c28aa2b3c17541bcb1868c24c
Akiranya <porridgen@gmail.com>
2024-07-10 19:19:52 +0800

    check missing entries in attributes.yml

---
+++ wakame-plugin/src/test/kotlin/attribute/AttributeConfigTest.kt
@@ -0,0 +1,75 @@
+package attribute
+
+import cc.mewcraft.wakame.adventure.adventureModule
+import cc.mewcraft.wakame.attribute.attributeModule
+import cc.mewcraft.wakame.config.Configs
+import cc.mewcraft.wakame.element.elementModule
+import cc.mewcraft.wakame.registry.ATTRIBUTE_GLOBAL_CONFIG_FILE
+import cc.mewcraft.wakame.registry.AttributeRegistry
+import cc.mewcraft.wakame.registry.ElementRegistry
+import cc.mewcraft.wakame.registry.registryModule
+import mainEnv
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.spongepowered.configurate.kotlin.extensions.contains
+import testEnv
+import kotlin.test.Test
+import kotlin.test.fail
+
+/**
+ * 用于检查所有属性的配置是否已经出现在 main/attributes.yml 或 test/attributes.yml.
+ */
+class AttributeConfigTest : KoinTest {
+    @BeforeEach
+    fun beforeEach() {
+        startKoin {
+            modules(
+                adventureModule(),
+                attributeModule(),
+                elementModule(),
+                registryModule(),
+            )
+        }
+    }
+
+    @AfterEach
+    fun afterEach() {
+        Configs.cleanup() // 清理所有已缓存的实例
+        stopKoin()
+    }
+
+    @Test
+    fun `check all attribute config's at main`() {
+        loadKoinModules(mainEnv())
+        checkMissingConfigs()
+    }
+
+    @Test
+    fun `check all attribute config's at test`() {
+        loadKoinModules(testEnv())
+        checkMissingConfigs()
+    }
+
+    private fun checkMissingConfigs() {
+        ElementRegistry.onPreWorld()
+        AttributeRegistry.onPreWorld()
+
+        val config = Configs.YAML[ATTRIBUTE_GLOBAL_CONFIG_FILE]
+
+        val missingConfigs = mutableListOf<String>()
+
+        for ((key, _) in AttributeRegistry.FACADES) {
+            if (!config.get().contains(key.value())) {
+                missingConfigs.add(key.asString())
+            }
+        }
+
+        if (missingConfigs.isNotEmpty()) {
+            fail("Missing attribute configs for: ${missingConfigs.joinToString(", ")}")
+        }
+    }
+}
