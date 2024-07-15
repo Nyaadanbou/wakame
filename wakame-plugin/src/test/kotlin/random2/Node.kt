09698c9ee7d35e2df4a3bbd02fd927420d406f88
Akiranya <porridgen@gmail.com>
2024-07-15 14:49:18 +0800

    finish up basic recursive data structure for `random2`

--- wakame-plugin/src/test/kotlin/random2/Node.kt
+++ wakame-plugin/src/test/kotlin/random2/Node.kt
@@ -2,35 +2,64 @@ package random2
 
 import net.kyori.adventure.key.Key
 
-sealed class Node<T>(open val key: Key)
+/**
+ * 代表一个节点.
+ *
+ * 节点可以自身储存任意值, 也可以引用一个全局的节点.
+ * 当节点引用一个全局节点时, 会递归解析全局节点的值.
+ */
+sealed interface Node<T> {
+    val key: Key
+}
 
 data class LocalNode<T>(
-    override val key: Key,
-    val value: T
-) : Node<T>(key)
+    override val key: Key, val value: T,
+) : Node<T>
 
 data class CompositeNode<T>(
-    override val key: Key,
-    private val nodes: MutableList<Node<T>> = mutableListOf()
-) : Node<T>(key) {
-    fun addNode(node: Node<T>): CompositeNode<T> {
-        nodes.add(node)
+    override val key: Key, private val nodes: MutableList<Node<T>> = mutableListOf(),
+) : Node<T> {
+    fun addNode(init: Builder<T>.() -> Unit): CompositeNode<T> {
+        val builder = Builder<T>().apply(init)
+        nodes.addAll(builder.nodes)
         return this
     }
 
-    fun getNodes(): List<Node<T>> = nodes
+    fun getNodes(): List<Node<T>> {
+        return nodes
+    }
+
+    class Builder<T> {
+        val nodes = mutableListOf<Node<T>>()
+
+        fun node(key: String, value: T) {
+            nodes.add(LocalNode(Key.key(key), value))
+        }
+
+        fun compositeNode(key: String, init: Builder<T>.() -> Unit = {}) {
+            val compositeNode = CompositeNode<T>(Key.key(key))
+            compositeNode.addNode(init)
+            nodes.add(compositeNode)
+        }
+    }
 }
 
-class NodeContainer<T>(private val globalDataSource: GlobalDataSource<T>) : Iterable<T> {
+class NodeContainer<T>(
+    private val shared: SharedStorage<T>,
+) : Iterable<T> {
     var root: Node<T>? = null
 
-    fun values(): List<T> = this.toList()
+    fun values(): List<T> {
+        return this.toList()
+    }
 
-    override fun iterator(): Iterator<T> = NodeIterator(root, globalDataSource)
+    override fun iterator(): Iterator<T> {
+        return NodeIterator(root, shared)
+    }
 
     private class NodeIterator<T>(
         root: Node<T>?,
-        private val globalDataSource: GlobalDataSource<T>
+        private val shared: SharedStorage<T>,
     ) : Iterator<T> {
         private val valuesQueue = mutableListOf<T>()
         private val visitedGlobals = mutableSetOf<String>()
@@ -41,12 +70,19 @@ class NodeContainer<T>(private val globalDataSource: GlobalDataSource<T>) : Iter
 
         private fun resolveNode(node: Node<T>) {
             when (node) {
-                is LocalNode -> valuesQueue.add(node.value)
+                is LocalNode -> {
+                    // 添加值到 valuesQueue 时,
+                    // 不检查值或 Node 的唯一性
+                    valuesQueue.add(node.value)
+                }
+
                 is CompositeNode -> {
-                    if (node.key.namespace() == "global" && visitedGlobals.add(node.key.value())) {
-                        globalDataSource.getNodesByKey(node.key.value()).forEach { resolveNode(it) }
+                    if (node.key.namespace() == SharedStorage.NAMESPACE_GLOBAL && visitedGlobals.add(node.key.value())) {
+                        for (it in shared.getNodesByKey(node.key.value())) {
+                            resolveNode(it)
+                        }
                     } else {
-                        node.getNodes().forEach { childNode ->
+                        for (childNode in node.getNodes()) {
                             resolveNode(childNode)
                         }
                     }
@@ -54,20 +90,29 @@ class NodeContainer<T>(private val globalDataSource: GlobalDataSource<T>) : Iter
             }
         }
 
-        override fun hasNext(): Boolean = valuesQueue.isNotEmpty()
+        override fun hasNext(): Boolean {
+            return valuesQueue.isNotEmpty()
+        }
 
         override fun next(): T {
-            if (!hasNext()) throw NoSuchElementException()
+            if (!hasNext()) {
+                throw NoSuchElementException()
+            }
             return valuesQueue.removeAt(0)
         }
     }
 }
 
-class GlobalDataSource<T> {
+class SharedStorage<T> {
+    companion object {
+        const val NAMESPACE_GLOBAL = "global"
+    }
+
     private val entries: HashMap<String, List<Node<T>>> = HashMap()
 
-    fun addEntry(key: String, nodes: List<Node<T>>) {
-        entries[key] = nodes
+    fun addEntry(key: String, init: Builder<T>.() -> Unit = {}) {
+        val builder = Builder<T>().apply(init)
+        entries[key] = builder.nodes
     }
 
     fun getNodesByKey(key: String): List<Node<T>> {
@@ -77,26 +122,44 @@ class GlobalDataSource<T> {
         return resolvedNodes
     }
 
-    private fun resolveNode(key: String, resolvedNodes: MutableList<Node<T>>, visited: MutableSet<String>) {
-        if (key in visited) return
-        visited.add(key)
+    private fun resolveNode(key: String, resolvedNodes: MutableList<Node<T>>, visitedKeys: MutableSet<String>) {
+        if (key in visitedKeys) {
+            return
+        }
+        visitedKeys.add(key)
 
         val nodes = entries[key] ?: return
-        nodes.forEach { node ->
+        for (node in nodes) {
             when (node) {
-                is LocalNode -> resolvedNodes.add(node)
+                is LocalNode -> {
+                    resolvedNodes.add(node)
+                }
+
                 is CompositeNode -> {
-                    if (!visited.contains(node.key.value())) {
-                        resolveNode(node.key.value(), resolvedNodes, visited)
+                    if (!visitedKeys.contains(node.key.value())) {
+                        resolveNode(node.key.value(), resolvedNodes, visitedKeys)
                     }
-                    node.getNodes().forEach { childNode ->
+                    for (childNode in node.getNodes()) {
                         when (childNode) {
                             is LocalNode -> resolvedNodes.add(childNode)
-                            is CompositeNode -> resolveNode(childNode.key.value(), resolvedNodes, visited)
+                            is CompositeNode -> resolveNode(childNode.key.value(), resolvedNodes, visitedKeys)
                         }
                     }
                 }
             }
+        }
+    }
+
+    class Builder<T> {
+        val nodes = mutableListOf<Node<T>>()
+
+        fun node(key: String, value: T) {
+            nodes.add(LocalNode(Key.key(key), value))
+        }
+
+        fun compositeNode(key: String, init: CompositeNode<T>.() -> Unit = {}) {
+            val compositeNode = CompositeNode<T>(Key.key(key)).apply(init)
+            nodes.add(compositeNode)
         }
     }
 }
--- wakame-plugin/src/test/kotlin/random2/NodeContainerTest.kt
+++ wakame-plugin/src/test/kotlin/random2/NodeContainerTest.kt
@@ -8,13 +8,16 @@ import kotlin.test.assertTrue
 class NodeContainerTest {
 
     @Test
-    fun testLocalNodeValues() {
-        val globalDataSource = GlobalDataSource<String>()
-        val nodeContainer = NodeContainer(globalDataSource)
+    fun `should resolve local node values correctly`() {
+        val sharedStorage = SharedStorage<String>()
+        val nodeContainer = NodeContainer(sharedStorage)
 
-        val rootNode = CompositeNode<String>(Key.key("root"))
-            .addNode(LocalNode(Key.key("foo:1"), value = "Value 1"))
-            .addNode(LocalNode(Key.key("foo:2"), value = "Value 2"))
+        val rootNode = CompositeNode<String>(Key.key("root")).apply {
+            addNode {
+                node("foo:1", "Value 1")
+                node("foo:2", "Value 2")
+            }
+        }
 
         nodeContainer.root = rootNode
 
@@ -23,26 +26,30 @@ class NodeContainerTest {
     }
 
     @Test
-    fun testGlobalNodeValues() {
-        val globalDataSource = GlobalDataSource<String>()
+    fun `should resolve global node values correctly`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("bar1") {
+                node("foo:3", "Global Value 3")
+            }
+            addEntry("bar2") {
+                node("foo:4", "Global Value 4")
+                node("foo:5", "Global Value 5")
+            }
+            addEntry("bar3") {
+                compositeNode("global:bar2")
+                node("foo:6", "Global Value 6")
+                node("foo:7", "Global Value 7")
+            }
+        }
 
-        globalDataSource.addEntry("bar1", listOf(LocalNode(Key.key("foo:3"), value = "Global Value 3")))
-        globalDataSource.addEntry("bar2", listOf(LocalNode(Key.key("foo:4"), value = "Global Value 4"), LocalNode(Key.key("foo:5"), value = "Global Value 5")))
-        globalDataSource.addEntry(
-            "bar3", listOf(
-                CompositeNode<String>(Key.key("global:bar2"))
-                    .addNode(LocalNode(Key.key("foo:6"), value = "Global Value 6"))
-                    .addNode(LocalNode(Key.key("foo:7"), value = "Global Value 7"))
-            )
-        )
+        val nodeContainer = NodeContainer(sharedStorage)
 
-        val nodeContainer = NodeContainer(globalDataSource)
-
-        val rootNode = CompositeNode<String>(Key.key("root"))
-            .addNode(LocalNode(Key.key("foo:1"), value = "Value 1"))
-            .addNode(LocalNode(Key.key("foo:2"), value = "Value 2"))
-            .addNode(CompositeNode(Key.key("global:bar1")))
-            .addNode(CompositeNode(Key.key("global:bar3")))
+        val rootNode = CompositeNode<String>(Key.key("root")).apply {
+            addNode { node("foo:1", "Value 1") }
+            addNode { node("foo:2", "Value 2") }
+            addNode { compositeNode("global:bar1") }
+            addNode { compositeNode("global:bar3") }
+        }
 
         nodeContainer.root = rootNode
 
@@ -51,19 +58,25 @@ class NodeContainerTest {
     }
 
     @Test
-    fun testMixedNodeValues() {
-        val globalDataSource = GlobalDataSource<String>()
+    fun `should resolve mixed node values correctly`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("bar1") {
+                node("foo:3", "Global Value 3")
+            }
+            addEntry("bar2") {
+                node("foo:4", "Global Value 4")
+                node("foo:5", "Global Value 5")
+            }
+        }
 
-        globalDataSource.addEntry("bar1", listOf(LocalNode(Key.key("foo:3"), value = "Global Value 3")))
-        globalDataSource.addEntry("bar2", listOf(LocalNode(Key.key("foo:4"), value = "Global Value 4"), LocalNode(Key.key("foo:5"), value = "Global Value 5")))
+        val nodeContainer = NodeContainer(sharedStorage)
 
-        val nodeContainer = NodeContainer(globalDataSource)
-
-        val rootNode = CompositeNode<String>(Key.key("root"))
-            .addNode(LocalNode(Key.key("foo:1"), value = "Value 1"))
-            .addNode(LocalNode(Key.key("foo:2"), value = "Value 2"))
-            .addNode(CompositeNode(Key.key("global:bar1")))
-            .addNode(CompositeNode(Key.key("global:bar2")))
+        val rootNode = CompositeNode<String>(Key.key("root")).apply {
+            addNode { node("foo:1", "Value 1") }
+            addNode { node("foo:2", "Value 2") }
+            addNode { compositeNode("global:bar1") }
+            addNode { compositeNode("global:bar2") }
+        }
 
         nodeContainer.root = rootNode
 
@@ -72,11 +85,186 @@ class NodeContainerTest {
     }
 
     @Test
-    fun testEmptyContainer() {
-        val globalDataSource = GlobalDataSource<String>()
-        val nodeContainer = NodeContainer(globalDataSource)
+    fun `should handle empty container without errors`() {
+        val sharedStorage = SharedStorage<String>()
+        val nodeContainer = NodeContainer(sharedStorage)
 
         val values = nodeContainer.values()
         assertTrue(values.isEmpty())
     }
+
+    @Test
+    fun `should correctly resolve single layer of nested nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("nested1") {
+                node("foo:nested1", "Nested Value 1")
+            }
+        }
+        val nodeContainer = NodeContainer(sharedStorage)
+
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:nested1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Nested Value 1"), values)
+    }
+
+    @Test
+    fun `should correctly resolve multiple layers of nested nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("nested1") {
+                compositeNode("global:nested2")
+            }
+            addEntry("nested2") {
+                node("foo:nested2", "Nested Value 2")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:nested1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Nested Value 2"), values)
+    }
+
+    @Test
+    fun `should handle empty global data source without errors`() {
+        val sharedStorage = SharedStorage<String>()
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root"))
+
+        val values = nodeContainer.values()
+        assertTrue(values.isEmpty())
+    }
+
+    @Test
+    fun `should include duplicate values from nested nodes with DSL`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("nested") {
+                node("foo:nested", "Duplicate Value")
+                node("foo:nested", "Duplicate Value")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:nested") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Duplicate Value", "Duplicate Value"), values)
+    }
+
+    @Test
+    fun `should correctly resolve deeply nested nodes across multiple global keys`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("nested1") {
+                compositeNode("global:nested2")
+            }
+            addEntry("nested2") {
+                compositeNode("global:nested3")
+            }
+            addEntry("nested3") {
+                node("foo:nested3", "Deeply Nested Value 3")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:nested1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Deeply Nested Value 3"), values)
+    }
+
+    @Test
+    fun `should correctly resolve single layer of nested composite nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("layer1") {
+                node("foo:layer1", "Layer 1 Value")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:layer1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Layer 1 Value"), values)
+    }
+
+    @Test
+    fun `should correctly resolve two layers of nested composite nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("layer2") {
+                node("foo:layer2", "Layer 2 Value")
+            }
+            addEntry("layer1") {
+                compositeNode("global:layer2")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:layer1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Layer 2 Value"), values)
+    }
+
+    @Test
+    fun `should correctly resolve three layers of nested composite nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("layer3") {
+                node("foo:layer3", "Layer 3 Value")
+            }
+            addEntry("layer2") {
+                compositeNode("global:layer3")
+            }
+            addEntry("layer1") {
+                compositeNode("global:layer2")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:layer1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Layer 3 Value"), values)
+    }
+
+    @Test
+    fun `should correctly resolve four layers of nested composite nodes`() {
+        val sharedStorage = SharedStorage<String>().apply {
+            addEntry("layer4") {
+                node("foo:layer4", "Layer 4 Value")
+            }
+            addEntry("layer3") {
+                compositeNode("global:layer4")
+            }
+            addEntry("layer2") {
+                compositeNode("global:layer3")
+            }
+            addEntry("layer1") {
+                compositeNode("global:layer2")
+            }
+        }
+
+        val nodeContainer = NodeContainer(sharedStorage)
+        nodeContainer.root = CompositeNode<String>(Key.key("root")).apply {
+            addNode { compositeNode("global:layer1") }
+        }
+
+        val values = nodeContainer.values()
+        assertEquals(listOf("Layer 4 Value"), values)
+    }
+
 }
