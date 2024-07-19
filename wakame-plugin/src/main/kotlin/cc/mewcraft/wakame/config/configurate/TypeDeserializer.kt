0c794a1aaf18c64fcc227adeeab99b43bf1b96b5
Akiranya <porridgen@gmail.com>
2024-07-19 13:30:27 +0800

    deprecate TypeDeserializer, in favor of TypeSerializer

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/config/configurate/TypeDeserializer.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/config/configurate/TypeDeserializer.kt
@@ -5,6 +5,7 @@ import org.spongepowered.configurate.ConfigurationOptions
 import org.spongepowered.configurate.serialize.TypeSerializer
 import java.lang.reflect.Type
 
+@Deprecated("Bad naming. Use TypeSerializer instead", replaceWith = ReplaceWith("TypeSerializer", "cc.mewcraft.wakame.config.configurate.TypeSerializer"))
 interface TypeDeserializer<T> : TypeSerializer<T> {
 
     override fun deserialize(type: Type, node: ConfigurationNode): T
