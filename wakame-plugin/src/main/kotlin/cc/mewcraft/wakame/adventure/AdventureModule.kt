98d44b05ff66dcbbaccadc5ac9345c9a96136774
g2213swo <2057581537@qq.com>
2024-07-23 09:17:46 +0000

    rename adventure_serializers to adventure_audience_message_serializers

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/adventure/AdventureModule.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/adventure/AdventureModule.kt
@@ -7,14 +7,14 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.spongepowered.configurate.serialize.TypeSerializerCollection
 
-const val ADVENTURE_SERIALIZERS = "adventure_serializers"
+const val ADVENTURE_AUDIENCE_MESSAGE_SERIALIZERS = "adventure_audience_message_serializers"
 
 internal fun adventureModule(): Module = module {
     includes(
         adventureTextModule(),
     )
 
-    single<TypeSerializerCollection>(named(ADVENTURE_SERIALIZERS)) {
+    single<TypeSerializerCollection>(named(ADVENTURE_AUDIENCE_MESSAGE_SERIALIZERS)) {
         TypeSerializerCollection.builder()
             .kregister(CombinedAudienceMessageSerializer)
             .kregister(AudienceMessageGroupSerializer)
