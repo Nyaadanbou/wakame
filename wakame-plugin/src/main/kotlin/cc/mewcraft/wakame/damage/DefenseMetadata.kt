d22806ada265018e4243cdc7c48336dfff189374
Akiranya <porridgen@gmail.com>
2024-07-25 23:15:53 +0800

    refactor & reformat code

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/damage/DefenseMetadata.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/damage/DefenseMetadata.kt
@@ -4,25 +4,25 @@ import cc.mewcraft.wakame.attribute.AttributeMap
 import cc.mewcraft.wakame.attribute.Attributes
 
 /**
- * 防御元数据
- * 包含了一次伤害中“防御阶段”的有关信息
- * 其实现类实例化时，最终伤害值以及各种信息就已经确定了
+ * 防御元数据.
+ * 包含了一次伤害中“防御阶段”的有关信息.
+ * 在实例化后, 最终伤害数值以及各种信息就已经确定了.
  */
 sealed interface DefenseMetadata {
     val damageeAttributeMap: AttributeMap
-    fun calculateFinalDamage(damageMetaData: DamageMetadata): Double
+    fun calculateFinalDamage(damageMetadata: DamageMetadata): Double
 }
 
 /**
- * 玩家和非玩家实体的防御元数据
+ * 玩家和非玩家实体的防御元数据.
  */
 class EntityDefenseMetadata(
     override val damageeAttributeMap: AttributeMap,
 ) : DefenseMetadata {
-    override fun calculateFinalDamage(damageMetaData: DamageMetadata): Double {
+    override fun calculateFinalDamage(damageMetadata: DamageMetadata): Double {
         var totalElementDamage = 0.0
-        val criticalPower = if (damageMetaData.isCritical) damageMetaData.criticalPower else 1.0
-        for (packet in damageMetaData.damageBundle.packets()) {
+        val criticalPower = if (damageMetadata.isCritical) damageMetadata.criticalPower else 1.0
+        for (packet in damageMetadata.damageBundle.packets()) {
             val element = packet.element
             val damageAfterDefense = DamageRules.calculateDamageAfterDefense(
                 packet.packetDamage,
@@ -30,8 +30,8 @@ class EntityDefenseMetadata(
                 packet.defensePenetration,
                 packet.defensePenetrationRate
             )
-            val incomingDamageRate = (damageeAttributeMap.getValue(Attributes.UNIVERSAL_INCOMING_DAMAGE_RATE) +
-                    damageeAttributeMap.getValue(Attributes.byElement(element).INCOMING_DAMAGE_RATE))
+            val incomingDamageRate = damageeAttributeMap.getValue(Attributes.UNIVERSAL_INCOMING_DAMAGE_RATE) +
+                    damageeAttributeMap.getValue(Attributes.byElement(element).INCOMING_DAMAGE_RATE)
             // 依次计算防御力、元素伤害百分比加成、考虑承伤百分比、最小伤害、暴击倍率
             totalElementDamage += (damageAfterDefense *
                     (1 + packet.rate) *
