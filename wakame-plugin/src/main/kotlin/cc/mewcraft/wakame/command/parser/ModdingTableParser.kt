aa9bd00fdffa233ec8c7b891dab6d9dc50cd9138
Akiranya <porridgen@gmail.com>
2024-07-27 13:12:41 +0800

    command can now specify table from config

---
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/command/parser/ModdingTableParser.kt
@@ -0,0 +1,48 @@
+package cc.mewcraft.wakame.command.parser
+
+import cc.mewcraft.wakame.reforge.modding.ModdingTable
+import cc.mewcraft.wakame.reforge.modding.ModdingTables
+import cc.mewcraft.wakame.util.typeTokenOf
+import org.incendo.cloud.caption.StandardCaptionKeys
+import org.incendo.cloud.component.CommandComponent
+import org.incendo.cloud.context.CommandContext
+import org.incendo.cloud.context.CommandInput
+import org.incendo.cloud.exception.parsing.ParserException
+import org.incendo.cloud.parser.ArgumentParseResult
+import org.incendo.cloud.parser.ArgumentParser
+import org.incendo.cloud.parser.ParserDescriptor
+import org.incendo.cloud.suggestion.BlockingSuggestionProvider
+
+class ModdingTableParser<C : Any> : ArgumentParser<C, ModdingTable>, BlockingSuggestionProvider.Strings<C> {
+    companion object Factory {
+        fun <C : Any> moddingTableParser(): ParserDescriptor<C, ModdingTable> {
+            return ParserDescriptor.of(ModdingTableParser(), typeTokenOf<ModdingTable>())
+        }
+
+        fun <C : Any> moddingTableComponent(): CommandComponent.Builder<C, ModdingTable> {
+            return CommandComponent.builder<C, ModdingTable?>().parser(moddingTableParser())
+        }
+    }
+
+    override fun parse(commandContext: CommandContext<C>, commandInput: CommandInput): ArgumentParseResult<ModdingTable> {
+        val peekStr = commandInput.peekString()
+        if (peekStr !in ModdingTables.NAMES) {
+            return ArgumentParseResult.failure(ModdingTableParseException(commandContext))
+        }
+
+        val readStr = commandInput.readString()
+        return ArgumentParseResult.success(ModdingTables[readStr]!!)
+    }
+
+    override fun stringSuggestions(commandContext: CommandContext<C>, input: CommandInput): Iterable<String> {
+        return ModdingTables.NAMES
+    }
+}
+
+class ModdingTableParseException(
+    context: CommandContext<*>,
+) : ParserException(
+    ModdingTableParser::class.java,
+    context,
+    StandardCaptionKeys.EXCEPTION_INVALID_ARGUMENT
+)
---
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/reforge/modding/ModdingType.kt
@@ -0,0 +1,13 @@
+package cc.mewcraft.wakame.reforge.modding
+
+/**
+ * 定制的类型.
+ *
+ * 目前有两种实现:
+ * - 定制核心
+ * - 定制诅咒
+ */
+enum class ModdingType {
+    CORE,
+    CURSE
+}
