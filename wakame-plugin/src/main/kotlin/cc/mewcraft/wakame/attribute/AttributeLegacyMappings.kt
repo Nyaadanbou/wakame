c02bd50a53a86e70b2490accce28c50f46184dde
Akiranya <porridgen@gmail.com>
2024-07-30 16:03:32 +0800

    improve kdoc

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attribute/AttributeLegacyMappings.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attribute/AttributeLegacyMappings.kt
@@ -6,9 +6,9 @@ import net.kyori.adventure.key.Key
 import java.util.UUID
 
 /**
- * 一个临时的实现, 用于将 [Key] 映射到 [UUID] 或者反过来.
+ * 一个临时的实现, 用于将 [Key] 映射到 [UUID], 或者反过来.
  *
- * 该实现保证了相同的 [Key] 会映射到相同的 [UUID], 并且逆映射亦是如此.
+ * 该实现保证相同的 [Key] 会映射到相同的 [UUID]; 反过来亦是如此.
  */
 object AttributeLegacyMappings {
     private val mappings: BiMap<Key, UUID> = HashBiMap.create()
