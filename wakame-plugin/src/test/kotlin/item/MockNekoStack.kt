0de9b8fdf97e59ec079d1b276e30d8348784fe3f
Akiranya <porridgen@gmail.com>
2024-08-08 14:22:45 +0800

    fix compilation

--- wakame-plugin/src/test/kotlin/item/MockNekoStack.kt
+++ wakame-plugin/src/test/kotlin/item/MockNekoStack.kt
@@ -1,6 +1,5 @@
 package item
 
-import cc.mewcraft.nbt.CompoundTag
 import cc.mewcraft.wakame.item.ItemSlotGroup
 import cc.mewcraft.wakame.item.NekoItem
 import cc.mewcraft.wakame.item.NekoStack
@@ -16,22 +15,33 @@ import java.util.stream.Stream
 class MockNekoStack(
     override val prototype: NekoItem,
 ) : NekoStack {
-    override val nbt: CompoundTag
-        get() = throw NotImplementedError("Not implemented")
-    override val handle: ItemStack
-        get() = throw NotImplementedError("Not implemented")
     override val itemStack: ItemStack
         get() = throw NotImplementedError("Not implemented")
+
     override val namespace: String = prototype.key.namespace()
+
     override val path: String = prototype.key.value()
+
     override val key: Key = prototype.key
+
     override var variant: Int = 0
+
     override val slotGroup: ItemSlotGroup = prototype.slotGroup
+
     override val components: ItemComponentMap = ItemComponentMap.builder().build()
+
     override val templates: ItemTemplateMap = prototype.templates
+
     override val behaviors: ItemBehaviorMap = prototype.behaviors
-    override fun clone(): NekoStack = throw NotImplementedError("Not implemented")
-    override fun erase() = Unit
+
+    override val unsafe: NekoStack.Unsafe
+        get() = throw NotImplementedError("Not implemented")
+
+    override fun clone(): NekoStack =
+        throw NotImplementedError("Not implemented")
+
+    override fun erase() =
+        Unit
 
     override fun examinableProperties(): Stream<out ExaminableProperty> = Stream.of(
         ExaminableProperty.of("key", key.asString()),
@@ -39,5 +49,6 @@ class MockNekoStack(
         ExaminableProperty.of("behaviors", behaviors),
     )
 
-    override fun toString(): String = toSimpleString()
+    override fun toString(): String =
+        toSimpleString()
 }
