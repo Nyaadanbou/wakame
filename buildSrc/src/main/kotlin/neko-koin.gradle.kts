60959555acb20a372227bd5f606ebb5d42cfbd01
Akiranya <porridgen@gmail.com>
2024-08-21 14:37:41 +0800

    remove ksp

--- buildSrc/src/main/kotlin/neko-koin.gradle.kts
+++ buildSrc/src/main/kotlin/neko-koin.gradle.kts
@@ -1,14 +1,5 @@
 plugins {
     `java-library`
-    id("com.google.devtools.ksp")
-}
-
-sourceSets.main {
-    java.srcDirs("build/generated/ksp/main/kotlin")
-}
-
-sourceSets.test {
-    java.srcDirs("build/generated/ksp/test/kotlin")
 }
 
 dependencies {
@@ -16,10 +7,6 @@ dependencies {
     implementation("io.insert-koin", "koin-core", Versions.KOIN_CORE) {
         exclude("org.jetbrains.kotlin")
     }
-    implementation("io.insert-koin", "koin-annotations", Versions.KOIN_ANNOTATIONS) {
-        exclude("org.jetbrains.kotlin")
-    }
-    ksp("io.insert-koin", "koin-ksp-compiler", Versions.KOIN_KSP)
 
     // test
     testImplementation("io.insert-koin", "koin-test", Versions.KOIN_CORE) {
