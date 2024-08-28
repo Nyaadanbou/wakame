8296d0f9879a15a2d2ddc6c30025ec3f573b9c1e
__FlandreScarlet <1903925559@qq.com>
2024-08-29 03:26:50 +0800

    add ArmorTrim component

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/components/ArmorTrim.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/components/ArmorTrim.kt
@@ -9,13 +9,21 @@ import cc.mewcraft.wakame.item.template.GenerationResult
 import cc.mewcraft.wakame.item.template.ItemTemplate
 import cc.mewcraft.wakame.item.template.ItemTemplateType
 import cc.mewcraft.wakame.util.editMeta
+import cc.mewcraft.wakame.util.krequire
 import cc.mewcraft.wakame.util.typeTokenOf
 import io.leangen.geantyref.TypeToken
+import io.papermc.paper.registry.RegistryAccess
+import io.papermc.paper.registry.RegistryKey
+import net.kyori.adventure.key.Key
 import net.kyori.examination.Examinable
 import org.bukkit.inventory.meta.ArmorMeta
+import org.bukkit.inventory.meta.trim.TrimMaterial
+import org.bukkit.inventory.meta.trim.TrimPattern
 import org.spongepowered.configurate.ConfigurationNode
 
 data class ArmorTrim(
+    val pattern: TrimPattern,
+    val material: TrimMaterial,
     val showInTooltip: Boolean,
 ) : Examinable {
 
@@ -34,12 +42,18 @@ data class ArmorTrim(
     ) : ItemComponentType<ArmorTrim> {
         override fun read(holder: ItemComponentHolder): ArmorTrim? {
             val im = holder.item.itemMeta as? ArmorMeta ?: return null
+            val trim = im.trim ?: return null
+
+            val pattern = trim.pattern
+            val material = trim.material
             val showInTooltip = !im.hasItemFlag(org.bukkit.inventory.ItemFlag.HIDE_ARMOR_TRIM)
-            return ArmorTrim(showInTooltip)
+
+            return ArmorTrim(pattern, material, showInTooltip)
         }
 
         override fun write(holder: ItemComponentHolder, value: ArmorTrim) {
             holder.item.editMeta<ArmorMeta> {
+                it.trim = org.bukkit.inventory.meta.trim.ArmorTrim(value.material, value.pattern)
                 if (value.showInTooltip) {
                     it.removeItemFlags(org.bukkit.inventory.ItemFlag.HIDE_ARMOR_TRIM)
                 } else {
@@ -56,12 +70,15 @@ data class ArmorTrim(
     }
 
     data class Template(
+        val pattern: TrimPattern,
+        val material: TrimMaterial,
         val showInTooltip: Boolean,
+        val mutable: Boolean,
     ) : ItemTemplate<ArmorTrim> {
         override val componentType: ItemComponentType<ArmorTrim> = ItemComponentTypes.TRIM
 
         override fun generate(context: GenerationContext): GenerationResult<ArmorTrim> {
-            return GenerationResult.of(ArmorTrim(showInTooltip))
+            return GenerationResult.of(ArmorTrim(pattern, material, showInTooltip))
         }
     }
 
@@ -74,12 +91,24 @@ data class ArmorTrim(
          * ## Node structure
          * ```yaml
          * <node>:
+         *   pattern: <key>
+         *   material: <key>
          *   show_in_tooltip: <boolean>
+         *   mutable: <boolean>
          * ```
          */
         override fun decode(node: ConfigurationNode): Template {
+            val patternKey = node.node("pattern").krequire<Key>()
+            val materialKey = node.node("material").krequire<Key>()
+            val pattern = RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_PATTERN).get(patternKey)
+            pattern ?: throw IllegalArgumentException("Unknown trim pattern key: '$patternKey'")
+            val material = RegistryAccess.registryAccess().getRegistry(RegistryKey.TRIM_MATERIAL).get(materialKey)
+            material ?: throw IllegalArgumentException("Unknown trim material key: '$materialKey'")
+
             val showInTooltip = node.node("show_in_tooltip").getBoolean(true)
-            return Template(showInTooltip)
+            val mutable = node.node("mutable").getBoolean(true)
+
+            return Template(pattern, material, showInTooltip, mutable)
         }
     }
 }
