a5b5ee4cdcab97a1b8667582017314b605c04cfc
Akiranya <porridgen@gmail.com>
2024-09-29 02:09:00 +0800

    add more documents

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment/EnchantmentEventHandler.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment/EnchantmentEventHandler.kt
@@ -8,7 +8,9 @@ import org.bukkit.inventory.ItemStack
 /**
  * 处理魔咒相关事件的处理器.
  *
- * 在 1.20.5 以前
+ * 在 1.20.5 以后, 可以通过数据包添加自定义的魔咒.
+ * 这也意味着我们不再需要手动处理与附魔相关的机制, 例如铁砧.
+ * 唯一需要处理的就是监听物品栏发生的变化, 以应用附魔的效果.
  */
 internal class EnchantmentEventHandler {
 
