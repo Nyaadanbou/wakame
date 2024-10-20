3aaf7f3dd92c502dac096073b252c36a145f2260
g2213swo <2057581537@qq.com>
2024-10-20 12:23:43 +0000

    remove HuskSync (unused)

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/user/UserModule.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/user/UserModule.kt
@@ -1,7 +1,5 @@
 package cc.mewcraft.wakame.user
 
-import cc.mewcraft.wakame.WakamePlugin
-import cc.mewcraft.wakame.compatibility.husksync.HuskSyncUserListener
 import cc.mewcraft.wakame.item.logic.AdventureLevelListener
 import org.bukkit.entity.Player
 import org.koin.core.module.Module
@@ -12,11 +10,5 @@ internal fun userModule(): Module = module {
     single<PlayerAdapter<Player>> { PaperPlayerAdapter() } binds arrayOf(PaperPlayerAdapter::class)
     single<UserManager<Player>> { PaperUserManager() } binds arrayOf(PaperUserManager::class)
 
-    single<UserListener> {
-        if (get<WakamePlugin>().isPluginPresent("HuskSync")) {
-            HuskSyncUserListener()
-        } else {
-            get<AdventureLevelListener>()
-        }
-    }
+    single<UserListener> { get<AdventureLevelListener>() }
 }
