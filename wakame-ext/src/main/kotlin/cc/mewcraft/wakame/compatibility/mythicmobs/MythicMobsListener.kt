662a5fd8ca35bda9d1fa783d5952e0ca4b65bda1
Akiranya <porridgen@gmail.com>
2024-10-25 22:42:45 +0800

    make it compile

--- wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/MythicMobsListener.kt
+++ wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/MythicMobsListener.kt
@@ -3,17 +3,12 @@ package cc.mewcraft.wakame.compatibility.mythicmobs
 import cc.mewcraft.wakame.compatibility.mythicmobs.condition.HasItemCondition
 import cc.mewcraft.wakame.compatibility.mythicmobs.condition.HoldingCondition
 import cc.mewcraft.wakame.compatibility.mythicmobs.drop.NekoDrop
-import cc.mewcraft.wakame.compatibility.mythicmobs.mechanic.AttributeMechanic
-import cc.mewcraft.wakame.compatibility.mythicmobs.mechanic.AttributeModifierMechanic
-import cc.mewcraft.wakame.compatibility.mythicmobs.mechanic.NekoDamageMechanic
-import cc.mewcraft.wakame.compatibility.mythicmobs.mechanic.RemoveAttributeModifierMechanic
+import cc.mewcraft.wakame.compatibility.mythicmobs.mechanic.*
 import io.lumine.mythic.api.config.MythicLineConfig
 import io.lumine.mythic.api.drops.IDrop
 import io.lumine.mythic.api.skills.ISkillMechanic
 import io.lumine.mythic.api.skills.conditions.ISkillCondition
-import io.lumine.mythic.bukkit.events.MythicConditionLoadEvent
-import io.lumine.mythic.bukkit.events.MythicDropLoadEvent
-import io.lumine.mythic.bukkit.events.MythicMechanicLoadEvent
+import io.lumine.mythic.bukkit.events.*
 import io.lumine.mythic.core.skills.SkillExecutor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -34,7 +29,7 @@ class MythicMobsListener : Listener {
         when (e.mechanicName.lowercase()) {
             "nekoattribute" -> e.registerMechanic(::AttributeMechanic)
             "nekoattributemodifier" -> e.registerMechanic(::AttributeModifierMechanic)
-            "nekodamage" -> e.registerMechanic(::NekoDamageMechanic)
+            // "nekodamage" -> e.registerMechanic(::NekoDamageMechanic)
             "nekoremoveattributemodifier" -> e.registerMechanic(::RemoveAttributeModifierMechanic)
         }
     }
--- wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/mechanic/NekoDamageMechanic.kt
+++ wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/mechanic/NekoDamageMechanic.kt
@@ -1,15 +1,9 @@
 package cc.mewcraft.wakame.compatibility.mythicmobs.mechanic
 
-import io.lumine.mythic.api.config.MythicLineConfig
-import io.lumine.mythic.api.skills.ITargetedEntitySkill
-import io.lumine.mythic.core.skills.SkillExecutor
-import io.lumine.mythic.core.skills.SkillMechanic
-import java.io.File
-
-class NekoDamageMechanic(
-    manager: SkillExecutor,
-    file: File,
-    line: String,
-    mlc: MythicLineConfig,
-) : SkillMechanic(manager, file, line, mlc), ITargetedEntitySkill {
-}
+// class NekoDamageMechanic(
+//     manager: SkillExecutor,
+//     file: File,
+//     line: String,
+//     mlc: MythicLineConfig,
+// ) : SkillMechanic(manager, file, line, mlc), ITargetedEntitySkill {
+// }
