bbf3378cc3d2410d6f35b9a661c1dd33f9c0efb8
Akiranya <porridgen@gmail.com>
2024-11-01 02:46:39 +0800

    add PriceModifierSerializer

---
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/reforge/common/PriceModifierSerializer.kt
@@ -0,0 +1,24 @@
+package cc.mewcraft.wakame.reforge.common
+
+import cc.mewcraft.wakame.config.configurate.TypeSerializer
+import cc.mewcraft.wakame.util.krequire
+import org.spongepowered.configurate.ConfigurationNode
+import java.lang.reflect.Type
+import kotlin.reflect.typeOf
+
+object PriceModifierSerializer : TypeSerializer<PriceModifier> {
+    private val TYPE_MAPPING = mapOf(
+        DamagePriceModifier.NAME to typeOf<DamagePriceModifier>(),
+        LevelPriceModifier.NAME to typeOf<LevelPriceModifier>(),
+        RarityPriceModifier.NAME to typeOf<RarityPriceModifier>(),
+        MergingPenaltyPriceModifier.NAME to typeOf<MergingPenaltyPriceModifier>(),
+        ModdingPenaltyPriceModifier.NAME to typeOf<ModdingPenaltyPriceModifier>(),
+        RerollingPenaltyPriceModifier.NAME to typeOf<RerollingPenaltyPriceModifier>(),
+    )
+
+    override fun deserialize(type: Type, node: ConfigurationNode): PriceModifier {
+        val nodeKey = node.key().toString()
+        val kType = TYPE_MAPPING[nodeKey] ?: throw IllegalArgumentException("Unknown price modifier type: $nodeKey")
+        return node.krequire(kType)
+    }
+}
