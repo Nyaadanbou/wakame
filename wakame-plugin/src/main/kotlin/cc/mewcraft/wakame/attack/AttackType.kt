cf23b0039c917cfee67f380d0e96afc27adf4e7a
Akiranya <porridgen@gmail.com>
2024-11-07 16:45:31 +0800

    fix tests

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attack/AttackType.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/attack/AttackType.kt
@@ -15,20 +15,22 @@ import org.bukkit.event.block.Action
 import org.koin.core.component.get
 import org.slf4j.Logger
 import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.ConfigurationOptions
 import java.lang.reflect.Type
 
 
 /**
  * 攻击类型.
- * 其实现类先不要写成单例.
- * 未来确认是无参数的实现再写成单例.
  */
+// 其实现类先不要写成单例.
+// 未来确认是无参数的实现再写成单例.
 sealed interface AttackType {
     /**
      * 玩家使用该攻击类型的物品直接左键攻击一个生物造成的伤害所使用的 [DamageMetadata].
      * 默认返回造成 1 点默认元素伤害的伤害元数据.
-     * 返回空后续会使本次伤害事件取消.
-     * !!!不要在该方法中的实现中写攻击的附带效果!!!
+     * 返回 `null` 后续会使本次伤害事件取消.
+     *
+     * !!! 不要在该方法中的实现中写攻击的附带效果 !!!
      */
     fun generateDamageMetadata(player: Player, nekoStack: NekoStack): DamageMetadata? {
         return PlayerDamageMetadata.HAND_WITHOUT_ATTACK
@@ -41,7 +43,7 @@ sealed interface AttackType {
 
     /**
      * 玩家使用该攻击类型的物品进行交互事件时执行的逻辑.
-     * 默认左键点击时触发攻击冷却
+     * 默认左键点击时触发攻击冷却.
      */
     fun handleInteract(player: Player, nekoStack: NekoStack, action: Action, wrappedEvent: WrappedPlayerInteractEvent) {
         if (!action.isLeftClick) return
@@ -56,6 +58,7 @@ sealed interface AttackType {
 
 /**
  * [AttackType] 的默认实现.
+ *
  * 实际表现为玩家徒手造成 1 默认元素伤害.
  */
 data object HandAttack : AttackType
@@ -65,6 +68,10 @@ data object HandAttack : AttackType
  */
 internal object AttackTypeSerializer : TypeSerializer<AttackType> {
     private val LOGGER: Logger = Injector.get()
+
+    override fun emptyValue(specificType: Type, options: ConfigurationOptions): AttackType {
+        return HandAttack // 默认的攻击类型
+    }
 
     override fun deserialize(type: Type, node: ConfigurationNode): AttackType {
         return when (
@@ -104,7 +111,7 @@ internal object AttackTypeSerializer : TypeSerializer<AttackType> {
             }
 
             else -> {
-                LOGGER.warn("Unknown attack type: '$attackType', using default attack type.")
+                LOGGER.warn("Unknown attack type: '$attackType', using default instead.")
                 HandAttack
             }
         }
