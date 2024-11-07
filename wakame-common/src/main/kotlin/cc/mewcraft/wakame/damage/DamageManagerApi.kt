035dcadf9e3d7604393fa627497b6a85bf31dcd2
Akiranya <porridgen@gmail.com>
2024-11-07 17:24:54 +0800

    improve doc

--- wakame-common/src/main/kotlin/cc/mewcraft/wakame/damage/DamageManagerApi.kt
+++ wakame-common/src/main/kotlin/cc/mewcraft/wakame/damage/DamageManagerApi.kt
@@ -6,12 +6,12 @@ import org.jetbrains.annotations.ApiStatus
 interface DamageManagerApi {
     /**
      * 对 [victim] 造成由 [damageMetadata] 指定的萌芽伤害.
-     * 当 [source] 为 `null` 时, 伤害属于无源, 即不会产生击退.
+     * 当 [source] 为 `null` 时, 伤害属于无源, 不会产生击退效果.
      *
-     * @param victim 被伤害的实体.
-     * @param damageMetadata 伤害元数据.
-     * @param source 伤害源.
-     * @param knockback 是否产生击退.
+     * @param victim 受到伤害的实体
+     * @param damageMetadata 伤害的元数据
+     * @param source 造成伤害的实体
+     * @param knockback 是否产生击退效果
      */
     fun hurt(
         victim: LivingEntity, damageMetadata: DamageMetadata,
