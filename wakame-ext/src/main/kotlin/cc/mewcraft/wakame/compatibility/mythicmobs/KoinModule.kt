2c0c80c2a132652bf0a9ca9c22c459f069450d85
Akiranya <porridgen@gmail.com>
2024-11-12 22:59:21 +0800

    forgot it

--- wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/KoinModule.kt
+++ wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/KoinModule.kt
@@ -1,8 +1,9 @@
 package cc.mewcraft.wakame.compatibility.mythicmobs
 
 import org.bukkit.event.Listener
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal fun mythicMobsModule() = module {
-    single<Listener> { MythicMobsListener() }
+    single { MythicMobsListener() } bind Listener::class
 }
