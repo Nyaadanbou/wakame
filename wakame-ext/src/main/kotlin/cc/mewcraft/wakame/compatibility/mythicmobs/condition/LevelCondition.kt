7764fdd3fc9be1c810a8c19a9636b8fd68680d4a
g2213swo <2057581537@qq.com>
2024-11-17 12:10:16 +0000

    push what we have

--- wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/condition/LevelCondition.kt
+++ wakame-ext/src/main/kotlin/cc/mewcraft/wakame/compatibility/mythicmobs/condition/LevelCondition.kt
@@ -3,11 +3,16 @@ package cc.mewcraft.wakame.compatibility.mythicmobs.condition
 import cc.mewcraft.adventurelevel.level.category.LevelCategory
 import cc.mewcraft.adventurelevel.plugin.AdventureLevelProvider
 import io.lumine.mythic.api.adapters.AbstractEntity
+import io.lumine.mythic.api.adapters.AbstractLocation
 import io.lumine.mythic.api.config.MythicLineConfig
 import io.lumine.mythic.api.skills.conditions.IEntityCondition
+import io.lumine.mythic.api.skills.conditions.ILocationCondition
+import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.bukkit.utils.numbers.RangedInt
 import io.lumine.mythic.core.skills.SkillCondition
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
+
 
 /**
  * 判断玩家的冒险等级.
@@ -15,11 +20,24 @@ import org.bukkit.entity.Player
 class LevelCondition(
     line: String,
     mlc: MythicLineConfig,
-) : SkillCondition(line), IEntityCondition {
+) : SkillCondition(line), IEntityCondition, ILocationCondition {
     private val level: RangedInt = RangedInt(mlc.getString(arrayOf("level"), ">0", *arrayOfNulls<String>(0)))
+    private val searchRadius: Double = mlc.getDouble(arrayOf("radius", "r"), .0)
 
     override fun check(target: AbstractEntity): Boolean {
-        val bukkitEntity = target.bukkitEntity
+        return checkLevel(target.bukkitEntity)
+    }
+
+    override fun check(target: AbstractLocation): Boolean {
+        val location = BukkitAdapter.adapt(target)
+
+        return location.getNearbyPlayers(searchRadius)
+            .firstOrNull()
+            ?.let { checkLevel(it) }
+            ?: false
+    }
+
+    private fun checkLevel(bukkitEntity: Entity): Boolean {
         if (bukkitEntity !is Player) {
             return false
         } else {
