66be51385b39fa0f72f8675c1b1febfb765ae17b
Nailm <porridgen@gmail.com>
2024-11-20 13:24:31 +0800

    添加新的附魔 (#284)
    * add new enchantments
    
    * refactor class property names
    
    * add missing enchantments (wtf)

---
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment/wrapper/implementation/Quake.kt
@@ -0,0 +1,24 @@
+package cc.mewcraft.wakame.enchantment.wrapper.implementation
+
+import cc.mewcraft.wakame.attribute.AttributeModifier
+import cc.mewcraft.wakame.attribute.Attributes
+import cc.mewcraft.wakame.enchantment.Enchantments
+import cc.mewcraft.wakame.enchantment.effects.EnchantmentEffect
+import cc.mewcraft.wakame.enchantment.wrapper.AbstractEnchantment
+import cc.mewcraft.wakame.enchantment.wrapper.EnchantmentAttributeComponent
+import cc.mewcraft.wakame.enchantment.wrapper.EnchantmentAttributeComponent.*
+import cc.mewcraft.wakame.item.ItemSlot
+
+internal class Quake : AbstractEnchantment(Enchantments.QUAKE) {
+    private val hammerDamageRangeAdd: EnchantmentAttributeComponent = EnchantmentAttributeComponent(
+        this, setOf(
+            Part(
+                Attributes.HAMMER_DAMAGE_RANGE, 0.5, 0.3, AttributeModifier.Operation.ADD
+            )
+        )
+    )
+
+    override fun getEffects(level: Int, slot: ItemSlot): Collection<EnchantmentEffect> {
+        return hammerDamageRangeAdd.getEffects(level, slot)
+    }
+}
---
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment/wrapper/implementation/SweepingEdge.kt
@@ -0,0 +1,24 @@
+package cc.mewcraft.wakame.enchantment.wrapper.implementation
+
+import cc.mewcraft.wakame.attribute.AttributeModifier
+import cc.mewcraft.wakame.attribute.Attributes
+import cc.mewcraft.wakame.enchantment.Enchantments
+import cc.mewcraft.wakame.enchantment.effects.EnchantmentEffect
+import cc.mewcraft.wakame.enchantment.wrapper.AbstractEnchantment
+import cc.mewcraft.wakame.enchantment.wrapper.EnchantmentAttributeComponent
+import cc.mewcraft.wakame.enchantment.wrapper.EnchantmentAttributeComponent.*
+import cc.mewcraft.wakame.item.ItemSlot
+
+internal class SweepingEdge : AbstractEnchantment(Enchantments.SWEEPING_EDGE) {
+    private val sweepingEdgeRatioAdd: EnchantmentAttributeComponent = EnchantmentAttributeComponent(
+        this, setOf(
+            Part(
+                Attributes.SWEEPING_DAMAGE_RATIO, 0.25, 0.15, AttributeModifier.Operation.ADD
+            )
+        )
+    )
+
+    override fun getEffects(level: Int, slot: ItemSlot): Collection<EnchantmentEffect> {
+        return sweepingEdgeRatioAdd.getEffects(level, slot)
+    }
+}
