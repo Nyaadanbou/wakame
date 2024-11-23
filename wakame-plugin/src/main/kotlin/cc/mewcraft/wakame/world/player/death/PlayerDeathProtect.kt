f0910901a032bb63df9001da801be52c2e4c94d3
Akiranya <porridgen@gmail.com>
2024-11-23 16:12:11 +0800

    add safeguard to prevent memory leak

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/world/player/death/PlayerDeathProtect.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/world/player/death/PlayerDeathProtect.kt
@@ -1,14 +1,17 @@
 package cc.mewcraft.wakame.world.player.death
 
 import cc.mewcraft.wakame.config.MAIN_CONFIG
-import com.github.benmanes.caffeine.cache.Cache
-import com.github.benmanes.caffeine.cache.Caffeine
+import com.github.benmanes.caffeine.cache.*
 import org.bukkit.entity.EntityType
 import org.bukkit.event.*
 import org.bukkit.event.entity.ItemSpawnEvent
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.slf4j.Logger
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 /**
  * 保护由玩家死亡掉落产生的物品实体.
@@ -17,11 +20,24 @@ import java.util.UUID
  * - 仅允许物品的所有者拾取掉落的物品
  *
  * 未来的功能:
+ * - 兼容其他系统???
  */
-class PlayerDeathProtect : Listener {
+class PlayerDeathProtect : Listener, KoinComponent {
+
+    private val logger: Logger = get()
 
     // 记录了将要掉落的物品堆叠, 以及物品对应的所有者
-    private val deathDropRecords: Cache<ItemStack, UUID> = Caffeine.newBuilder().build()
+    private val deathDropRecords: Cache<ItemStack, UUID> = Caffeine.newBuilder()
+        // 为了防止任何潜在的内存泄漏, 设置个保底的过期时间.
+        // 理论上一旦玩家死亡爆了物品, 那么缓存应该就立马会被读取, 并被无效化.
+        // 理论上这个过期永远都不应该自动触发, 如果是那应该是 BUG.
+        .expireAfterWrite(10, TimeUnit.SECONDS)
+        .removalListener<ItemStack, UUID> { key, value, cause ->
+            if (cause.wasEvicted() && cause != RemovalCause.EXPLICIT) {
+                logger.warn("ItemStack $key was evicted (but not explicitly) from cache. This is a bug!")
+            }
+        }
+        .build()
 
     private val onlyOwnerCanPickupDeathDrops: Boolean by MAIN_CONFIG.entry("only_owner_can_pickup_death_drops")
 
