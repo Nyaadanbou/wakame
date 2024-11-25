44a52fd7cc8c3357e6dbc07829042999e45aa8ad
Akiranya <porridgen@gmail.com>
2024-11-25 20:47:02 +0800

    log hook loading status

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/integration/HooksLoader.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/integration/HooksLoader.kt
@@ -15,6 +15,9 @@ import cc.mewcraft.wakame.integration.playerlevel.PlayerLevelManager
 import cc.mewcraft.wakame.integration.playerlevel.PlayerLevelType
 import cc.mewcraft.wakame.integration.protection.ProtectionManager
 import cc.mewcraft.wakame.util.data.JarUtils
+import net.kyori.adventure.extra.kotlin.text
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger
 import org.bukkit.Bukkit
 import org.koin.core.component.get
 import org.objectweb.asm.Type
@@ -36,7 +39,7 @@ internal object HooksLoader : Initializable {
     // 部分 integration 会在 pre world 阶段加载默认(自带)的钩子,
     // 因此对于外部系统, 应该在 post world 阶段加载它们的钩子,
     // 这样的话外部的钩子实例就可以覆盖默认的钩子实例.
-    override suspend fun onPostWorldAsync() {
+    override fun onPostWorld() {
         loadHooks()
     }
 
@@ -60,6 +63,11 @@ internal object HooksLoader : Initializable {
 
                 if (shouldLoadHook(plugins, unless, requireAll)) {
                     loadHook(className.replace('/', '.'), loadListener)
+
+                    // 记录加载的钩子
+                    Injector.get<ComponentLogger>().info(text {
+                        content("Hook ${className.substringAfterLast('/')} loaded"); color(NamedTextColor.AQUA)
+                    })
                 }
             } catch (t: Throwable) {
                 Injector.get<Logger>().error("Failed to load hook $className", t)
