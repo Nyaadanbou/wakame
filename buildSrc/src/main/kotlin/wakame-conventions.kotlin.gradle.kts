98e3f43dc83b158f1fd1701aeb51e825655786c0
Akiranya <porridgen@gmail.com>
2024-11-25 19:58:01 +0800

    fix `configurate` conflict in test environment

--- buildSrc/src/main/kotlin/wakame-conventions.kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/wakame-conventions.kotlin.gradle.kts
@@ -6,6 +6,18 @@ plugins {
 // Expose version catalog
 val local = the<org.gradle.accessors.dm.LibrariesForLocal>()
 
+configurations.all {
+    // paperweight 会把 paper 自带的 configurate 加入到 test classpath,
+    // 然后一般的 exclude 对于 paperweight.paperDevBundle 没有任何效果.
+    // 这导致了 paper 的 configurate 和我们自己的 fork 在 test 环境发生冲突.
+    // 不过在服务端上直接跑没有这个问题 (因为 plugin classloader 有更高优先级).
+    //
+    // 使用以下代码直接移除所有的 spongepowered configurate 依赖.
+    exclude("org.spongepowered", "configurate-core")
+    exclude("org.spongepowered", "configurate-yaml")
+    exclude("org.spongepowered", "configurate-gson")
+}
+
 tasks {
     compileJava {
         options.encoding = "UTF-8"
