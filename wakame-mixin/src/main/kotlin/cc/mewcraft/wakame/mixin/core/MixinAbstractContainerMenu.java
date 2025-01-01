393c48a2738a86a2350fcbdba2fbbf77cc6fa1a5
g2213swo <2057581537@qq.com>
2025-01-01 18:55:48 +0800

    remove useless logic

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinAbstractContainerMenu.java
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinAbstractContainerMenu.java
@@ -1,10 +1,8 @@
 package cc.mewcraft.wakame.mixin.core;
 
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerSynchronizer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.CustomData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,10 +31,6 @@ public abstract class MixinAbstractContainerMenu {
         AbstractContainerMenu container = (AbstractContainerMenu) (Object) this;
         if (!this.suppressRemoteUpdates) {
             if (!ItemStack.matches(this.getCarried(), this.remoteCarried)) {
-                CustomData customData = this.remoteCarried.get(DataComponents.CUSTOM_DATA);
-                if (customData != null && customData.contains("processed")) {
-                    return;
-                }
                 this.remoteCarried = this.getCarried().copy();
                 if (this.synchronizer != null) {
                     this.synchronizer.sendCarriedChange(container, this.remoteCarried);
