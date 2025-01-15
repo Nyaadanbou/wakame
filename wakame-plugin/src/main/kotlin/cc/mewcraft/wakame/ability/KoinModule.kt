03640da983688cb4e0cf1a9942f5899868cff076
g2213swo <2057581537@qq.com>
2025-01-15 15:40:22 +0800

    fix rebase

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ability/KoinModule.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ability/KoinModule.kt
@@ -6,7 +6,6 @@ import cc.mewcraft.wakame.ability.state.display.PlayerStateDisplay
 import cc.mewcraft.wakame.ability.state.display.StateDisplay
 import cc.mewcraft.wakame.ability.trigger.AbilityTriggerSerializer
 import cc.mewcraft.wakame.ability.trigger.abilityTriggerModule
-import cc.mewcraft.wakame.molang.EVALUABLE_SERIALIZERS
 import cc.mewcraft.wakame.util.register
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
