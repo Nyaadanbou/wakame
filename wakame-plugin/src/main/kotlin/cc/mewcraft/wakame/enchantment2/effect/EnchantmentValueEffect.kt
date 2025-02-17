0544f5e6868543e23fd523cb21890d7f87165980
Ciallo <porridgen@gmail.com>
2025-02-18 03:36:04 +0800

    remove unused code

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment2/effect/EnchantmentValueEffect.kt
+++
@@ -1,9 +0,0 @@
-package cc.mewcraft.wakame.enchantment2.effect
-
-import kotlin.random.Random
-
-interface EnchantmentValueEffect {
-
-    fun apply(level: Int, random: Random, inputValue: Float): Float
-
-}
--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment2/wrapper/KoishEnchantmentMark.kt
+++
@@ -1,4 +0,0 @@
-package cc.mewcraft.wakame.enchantment2.wrapper
-
-@Target(AnnotationTarget.CLASS)
-annotation class KoishEnchantmentMark
