05f31fddf3200fdabf679dda7a75744c86071605
g2213swo <2057581537@qq.com>
2025-03-02 21:51:59 +0800

    optimize user experience

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ability/archetype/implement/ExtraJump.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ability/archetype/implement/ExtraJump.kt
@@ -11,6 +11,7 @@ import cc.mewcraft.wakame.adventure.AudienceMessageGroup
 import cc.mewcraft.wakame.ecs.Mechanic
 import cc.mewcraft.wakame.ecs.data.ParticleInfo
 import cc.mewcraft.wakame.ecs.data.SpiralPath
+import cc.mewcraft.wakame.ecs.data.TickResult
 import cc.mewcraft.wakame.ecs.external.ComponentBridge
 import cc.mewcraft.wakame.util.event.Events
 import cc.mewcraft.wakame.util.event.Subscription
@@ -47,21 +48,35 @@ private class ExtraJump(
 private class ExtraJumpAbilityMechanic(
     private val extraJump: ExtraJump,
 ) : PassiveAbilityMechanic() {
+    companion object {
+        private const val USE_COOLDOWN = 5L
+    }
+
     private lateinit var jumpSubscription: Subscription
     private lateinit var touchGroundSubscription: Subscription
 
+    private var cooldown: Long = USE_COOLDOWN
     private var jumpCount: Int = extraJump.count
+
+    override fun tick(deltaTime: Double, tickCount: Double, componentBridge: ComponentBridge): TickResult {
+        if (cooldown > 0) {
+            cooldown--
+        }
+        return super.tick(deltaTime, tickCount, componentBridge)
+    }
 
     override fun onEnable(componentBridge: ComponentBridge) = abilitySupport {
         jumpSubscription = Events.subscribe(PlayerInputEvent::class.java)
             .filter { it.player == componentBridge.castByEntity() }
             .filter { jumpCount > 0 }
-            .filter { it.input.isJump }
+            .filter { cooldown <= 0 }
+            .filter { it.input.isJump.also { if (it) cooldown = USE_COOLDOWN } }
             .filter { @Suppress("DEPRECATION") !it.player.isOnGround } // Spigot 弃用此方法的原因是客户端可以通过发包来欺骗服务端, 我们这里不考虑挂端欺骗的情况.
             .handler {
                 // 进行额外的跳跃效果, 也就是让玩家在跳跃的时候额外的向上移动一段距禽.
                 val player = it.player
-                player.velocity = player.velocity.setY(0.6)
+                val direction = player.location.direction.normalize()
+                player.velocity = player.velocity.add(direction.multiply(0.3)).setY(0.6)
                 // 减少跳跃次数.
                 jumpCount--
 
