a871ec407261321e3cfbfee0cf87dcee8d04ad69
__FlandreScarlet <1903925559@qq.com>
2025-03-04 00:22:16 +0800

    bug fix

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/shadow/loot/ShadowTagEntry.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/shadow/loot/ShadowTagEntry.kt
@@ -4,9 +4,9 @@ import me.lucko.shadow.ClassTarget
 import me.lucko.shadow.Field
 import me.lucko.shadow.Shadow
 import me.lucko.shadow.Target
-import net.minecraft.tags.TagEntry
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Item
+import net.minecraft.world.level.storage.loot.entries.TagEntry
 
 @ClassTarget(TagEntry::class)
 internal interface ShadowTagEntry : Shadow {
