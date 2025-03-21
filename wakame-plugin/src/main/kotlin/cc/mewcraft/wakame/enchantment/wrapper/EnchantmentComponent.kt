4dfbb37ffc1f8f6f24884fa17390b33d9603d851
Ciallo <porridgen@gmail.com>
2025-03-21 13:52:15 +0800

    移除旧附魔实现

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/enchantment/wrapper/EnchantmentComponent.kt
+++
@@ -1,7 +0,0 @@
-package cc.mewcraft.wakame.enchantment.wrapper
-
-import cc.mewcraft.wakame.enchantment.CustomEnchantment
-
-internal abstract class EnchantmentComponent(
-    protected val owner: CustomEnchantment,
-)
