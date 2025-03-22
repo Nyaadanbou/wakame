375d7ff8535cdcf0789a810102e511f0b62515a0
Ciallo <porridgen@gmail.com>
2025-03-23 03:31:24 +0800

    add @JvmField

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ecs/MetadataKeys.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/ecs/MetadataKeys.kt
@@ -5,8 +5,13 @@ import cc.mewcraft.wakame.util.metadata.MetadataKey
 
 object MetadataKeys {
 
+    @JvmField
     val ECS_BUKKIT_PLAYER_ENTITY_ID = MetadataKey.create("ecs_bukkit_player_entity_id", KoishEntity::class.java)
+
+    @JvmField
     val ECS_BUKKIT_BLOCK_ENTITY_ID = MetadataKey.create("ecs_bukkit_block_entity_id", KoishEntity::class.java)
+
+    @JvmField
     val ECS_BUKKIT_ENTITY_ENTITY_ID = MetadataKey.create("ecs_bukkit_entity_entity_id", KoishEntity::class.java)
 
 }
