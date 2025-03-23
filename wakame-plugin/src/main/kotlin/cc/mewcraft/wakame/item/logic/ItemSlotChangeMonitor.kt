2f5e4498a658b79e15980b74e54698c258d2e873
Ciallo <porridgen@gmail.com>
2025-03-24 00:27:53 +0800

    简化 ItemSlotChangeMonitor

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/logic/ItemSlotChangeMonitor.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/item/logic/ItemSlotChangeMonitor.kt
@@ -17,16 +17,14 @@ import com.github.quillraven.fleks.IteratingSystem
  * 具体来说:
  * 我们每 tick 扫描每个玩家背包里的特定槽位上的物品.
  * 如果第 `n` tick 扫描的结果和第 `n-1` tick 扫描的结果不同,
- * 则认为这个槽位发生了变化, 那么此时就会触发一个事件.
+ * 则认为这个槽位发生了变化, 那么此时就会记录并触发一个事件.
  */
 class ItemSlotChangeMonitor : IteratingSystem(
     family = Families.BUKKIT_PLAYER
 ), FamilyOnAdd {
     override fun onTickEntity(entity: Entity) {
-        val everyItemSlot = ItemSlotRegistry.all()
-        val player = entity[BukkitPlayerComponent].bukkitPlayer
-        val user = player.toUser()
-        if (!user.isInventoryListenable) {
+        val bukkitPlayer = entity[BukkitPlayerComponent].bukkitPlayer
+        if (!bukkitPlayer.toUser().isInventoryListenable) {
             // 当玩家的背包不可监听时, 跳过扫描, 跳过触发事件.
             // 换句话说, 在 isInventoryListenable 为 false 时,
             // ItemSlotChanges 永远不会更新, 并且 isEmpty 为 true.
@@ -35,13 +33,12 @@ class ItemSlotChangeMonitor : IteratingSystem(
             return
         }
 
-        for (itemSlot in everyItemSlot) {
-            val currItemStack = itemSlot.getItem(player)
-            val itemSlotChangesEntry = entity[ItemSlotChanges][itemSlot]
-            itemSlotChangesEntry.update(currItemStack)
-            if (itemSlotChangesEntry.changing) {
-                val changeEvent = PlayerItemSlotChangeEvent(player, itemSlot, itemSlotChangesEntry.previous, itemSlotChangesEntry.current)
-                changeEvent.callEvent()
+        for (slot in ItemSlotRegistry.all()) {
+            val curr = slot.getItem(bukkitPlayer)
+            val entry = entity[ItemSlotChanges][slot]
+            entry.update(curr) // TODO 优化这里的性能
+            if (entry.changing) {
+                PlayerItemSlotChangeEvent(bukkitPlayer, slot, entry.previous, entry.current).callEvent()
             }
         }
     }
