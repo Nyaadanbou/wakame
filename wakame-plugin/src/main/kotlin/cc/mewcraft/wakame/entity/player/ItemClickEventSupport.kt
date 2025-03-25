1c05e645b4bd9b988b0a11f672dc93d7683b500e
Ciallo <porridgen@gmail.com>
2025-03-25 18:48:33 +0800

    对于本身存在交互的操作不触发事件

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/entity/player/ItemClickEventSupport.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/entity/player/ItemClickEventSupport.kt
@@ -10,17 +10,11 @@ import cc.mewcraft.wakame.util.registerEvents
 import com.destroystokyo.paper.event.server.ServerTickStartEvent
 import it.unimi.dsi.fastutil.objects.Reference2BooleanOpenHashMap
 import org.bukkit.Tag
-import org.bukkit.entity.ArmorStand
-import org.bukkit.entity.Boat
-import org.bukkit.entity.Minecart
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
-
-// FIXME #363: 随便写了点实现, 需要完善/修正这里的逻辑
 
 /**
  * 本 object 负责实现:
@@ -33,14 +27,6 @@ internal object ItemClickEventSupport : Listener {
     @InitFun
     fun init() {
         registerEvents()
-    }
-
-    // FIXME #363: 手持物品左键 Boat/Minecart 不会触发 EntityDamageEvent
-    @EventHandler
-    fun on(event: EntityDamageEvent) {
-        val player = event.damageSource.directEntity as? Player ?: return
-        val itemInMainHand = player.inventory.itemInMainHand.takeUnlessEmpty() ?: return
-        PlayerItemLeftClickEvent(player, itemInMainHand).callEvent()
     }
 
     // 记录了当前 tick 右键点击过 Boat 的玩家
@@ -76,19 +62,28 @@ internal object ItemClickEventSupport : Listener {
         }
     }
 
+    // 注意: 手持物品左键 Boat/Minecart 不会触发 EntityDamageEvent
     @EventHandler
-    fun on(event: PlayerInteractAtEntityEvent) {
-        val player = event.player
-        val hand = event.hand
-        val item = player.inventory.getItem(hand).takeUnlessEmpty() ?: return
-        val rightClicked = event.rightClicked
-        if (rightClicked is Boat ||
-            rightClicked is Minecart ||
-            rightClicked is ArmorStand
-        ) {
-            // 仅处理 Boat, Minecart, ArmorStand
-            PlayerItemRightClickEvent(player, item, hand).callEvent()
-        }
+    fun on(event: EntityDamageEvent) {
+        val player = event.damageSource.directEntity as? Player ?: return
+        val itemInMainHand = player.inventory.itemInMainHand.takeUnlessEmpty() ?: return
+        PlayerItemLeftClickEvent(player, itemInMainHand).callEvent()
     }
+
+    // 经讨论, 对于本身存在交互的操作(如右键船,矿车,盔甲架)不触发事件
+    //@EventHandler
+    //fun on(event: PlayerInteractAtEntityEvent) {
+    //    val player = event.player
+    //    val hand = event.hand
+    //    val item = player.inventory.getItem(hand).takeUnlessEmpty() ?: return
+    //    val rightClicked = event.rightClicked
+    //    if (rightClicked is Boat ||
+    //        rightClicked is Minecart ||
+    //        rightClicked is ArmorStand
+    //    ) {
+    //        // 仅处理 Boat, Minecart, ArmorStand
+    //        PlayerItemRightClickEvent(player, item, hand).callEvent()
+    //    }
+    //}
 
 }
