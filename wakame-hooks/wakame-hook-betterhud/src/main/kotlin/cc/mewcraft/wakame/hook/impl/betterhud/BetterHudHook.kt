d8967a6e58b70a57c8c7cdae049e8783b1389d78
g2213swo <2057581537@qq.com>
2025-04-06 19:45:48 +0800

    add attribute placeholders to BetterHud and PlaceholderAPI integration

--- wakame-hooks/wakame-hook-betterhud/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betterhud/BetterHudHook.kt
+++ wakame-hooks/wakame-hook-betterhud/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betterhud/BetterHudHook.kt
@@ -1,20 +1,27 @@
 package cc.mewcraft.wakame.hook.impl.betterhud
 
 import cc.mewcraft.wakame.SERVER
+import cc.mewcraft.wakame.attribute.Attributes
 import cc.mewcraft.wakame.ecs.bridge.koishify
+import cc.mewcraft.wakame.ecs.component.AttributeMapComponent
 import cc.mewcraft.wakame.ecs.component.Mana
 import cc.mewcraft.wakame.integration.Hook
 import kr.toxicity.hud.api.BetterHud
 import kr.toxicity.hud.api.BetterHudAPI
 import kr.toxicity.hud.api.manager.PlaceholderManager
 import kr.toxicity.hud.api.placeholder.HudPlaceholder
+import java.util.function.Function
 
 @Hook(plugins = ["BetterHud"])
 object BetterHudHook {
     val betterHud: BetterHud = BetterHudAPI.inst()
-    val placeholderManager: PlaceholderManager = betterHud.placeholderManager
 
     init {
+        registerPlaceholders()
+    }
+
+    private fun registerPlaceholders() {
+        val placeholderManager: PlaceholderManager = betterHud.placeholderManager
         placeholderManager.numberContainer.addPlaceholder(
             "koish_mana", HudPlaceholder.of(
                 HudPlaceholder.PlaceholderFunction.of { player ->
@@ -44,6 +51,20 @@ object BetterHudHook {
                     }
                 }
             )
+        )
+        placeholderManager.numberContainer.addPlaceholder(
+            "koish_attribute", HudPlaceholder.builder<Number>()
+                .requiredArgsLength(1)
+                .function { args, updateEvent ->
+                    Function { player ->
+                        val bukkitPlayer = requireNotNull(SERVER.getPlayer(player.uuid())) { "Player ${player.uuid()} not found" }
+                        val attributeMap = bukkitPlayer.koishify()[AttributeMapComponent]()
+                        val attribute = Attributes.get(args[0]) ?: return@Function 0
+                        attributeMap.getValue(attribute)
+                    }
+                }
+                .build()
+
         )
     }
 }
--- wakame-hooks/wakame-hook-papi/src/main/kotlin/cc/mewcraft/wakame/hook/impl/papi/PlaceholderAPIHook.kt
+++ wakame-hooks/wakame-hook-papi/src/main/kotlin/cc/mewcraft/wakame/hook/impl/papi/PlaceholderAPIHook.kt
@@ -2,6 +2,7 @@ package cc.mewcraft.wakame.hook.impl.papi
 
 import cc.mewcraft.wakame.BootstrapContexts
 import cc.mewcraft.wakame.ecs.bridge.koishify
+import cc.mewcraft.wakame.ecs.component.AttributeMapComponent
 import cc.mewcraft.wakame.ecs.component.Mana
 import cc.mewcraft.wakame.integration.Hook
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
@@ -21,7 +22,8 @@ object PlaceholderAPIHook : PlaceholderExpansion() {
     override fun persist(): Boolean = true
 
     override fun onPlaceholderRequest(player: Player, params: String): String? {
-        when (params) {
+        val paramsList = params.lowercase().split("_", limit = 2)
+        when (paramsList[0]) {
             "mana" -> {
                 val current = player.koishify()[Mana].current
                 return current.toString()
@@ -40,6 +42,13 @@ object PlaceholderAPIHook : PlaceholderExpansion() {
                 } else {
                     .0.toString()
                 }
+            }
+            "attribute" -> {
+                if (paramsList.size < 2)
+                    return null
+                val attributeMap = player.koishify()[AttributeMapComponent]()
+                val attribute = Attributes.get(paramsList[1]) ?: return null
+                return attributeMap.getValue(attribute).toString()
             }
         }
         return null
