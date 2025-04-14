3a3d60272f76d081b0f5214aa17d3c275c96cdc5
Ciallo <porridgen@gmail.com>
2025-04-14 17:52:09 +0800

    添加了 EconomyBridge 对插件 `Economy` 的支持

--- wakame-hooks/wakame-hook-economybridge/src/main/kotlin/cc/mewcraft/wakame/hook/impl/economybridge/EconomyBridgeHook.kt
+++ wakame-hooks/wakame-hook-economybridge/src/main/kotlin/cc/mewcraft/wakame/hook/impl/economybridge/EconomyBridgeHook.kt
@@ -1,18 +1,36 @@
 package cc.mewcraft.wakame.hook.impl.economybridge
 
+import cc.mewcraft.economy.api.EconomyProvider
 import cc.mewcraft.wakame.integration.Hook
 import cc.mewcraft.wakame.item2.KoishItemRefHandler
 import cc.mewcraft.wakame.util.Identifiers
+import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import su.nightexpress.economybridge.EconomyBridge
 import su.nightexpress.economybridge.ItemBridge
+import su.nightexpress.economybridge.api.Currency
 import su.nightexpress.economybridge.item.handler.AbstractItemHandler
+import java.util.*
+import cc.mewcraft.economy.api.Currency as KoishCurrency
 
 @Hook(plugins = ["EconomyBridge"])
-object EconomyBridgeHook : AbstractItemHandler() {
+object EconomyBridgeHook {
 
     init {
-        ItemBridge.getItemManager().register(this)
+        // 向 EconomyBridge 注册 Koish 物品
+        ItemBridge.getItemManager().register(KoishItemHandler)
+
+        // 向 EconomyBridge 注册 Koish 货币
+        // FIXME 这样注册的货币会在 EconomyBridge 执行重载指令时失效
+        for (koishCurrency in EconomyProvider.get().loadedCurrencies) {
+            EconomyBridge.getCurrencyManager().registerCurrency(EconomyCurrency(koishCurrency))
+        }
     }
+
+}
+
+object KoishItemHandler : AbstractItemHandler() {
 
     override fun getName(): String {
         return KoishItemRefHandler.systemName
@@ -35,5 +53,73 @@ object EconomyBridgeHook : AbstractItemHandler() {
         val id = Identifiers.of(p0)
         return KoishItemRefHandler.accepts(id)
     }
+}
 
+class EconomyCurrency(
+    private val currency: KoishCurrency,
+) : Currency {
+
+    override fun getBalance(p0: Player): Double {
+        return EconomyProvider.get().getBalance(p0.uniqueId, currency)
+    }
+
+    override fun getBalance(p0: UUID): Double {
+        return EconomyProvider.get().getBalance(p0, currency)
+    }
+
+    override fun give(p0: Player, p1: Double) {
+        EconomyProvider.get().deposit(p0.uniqueId, p1, currency)
+    }
+
+    override fun give(p0: UUID, p1: Double) {
+        EconomyProvider.get().deposit(p0, p1, currency)
+    }
+
+    override fun take(p0: Player, p1: Double) {
+        EconomyProvider.get().withdraw(p0.uniqueId, p1, currency)
+    }
+
+    override fun take(p0: UUID, p1: Double) {
+        EconomyProvider.get().withdraw(p0, p1, currency)
+    }
+
+    override fun format(amount: Double): String {
+        return currency.fancyFormat(amount)
+    }
+
+    override fun canHandleDecimals(): Boolean {
+        return currency.isDecimalSupported
+    }
+
+    override fun canHandleOffline(): Boolean {
+        return true
+    }
+
+    override fun getOriginalId(): String {
+        return currency.name
+    }
+
+    override fun getInternalId(): String {
+        return currency.name.lowercase()
+    }
+
+    override fun getName(): String {
+        return currency.displayName
+    }
+
+    override fun getDefaultName(): String {
+        return currency.displayName
+    }
+
+    override fun getFormat(): String {
+        return "@" // 这函数是干嘛用的?
+    }
+
+    override fun getIcon(): ItemStack {
+        return ItemStack(Material.SUNFLOWER)
+    }
+
+    override fun getDefaultIcon(): ItemStack {
+        return ItemStack(Material.SUNFLOWER)
+    }
 }
