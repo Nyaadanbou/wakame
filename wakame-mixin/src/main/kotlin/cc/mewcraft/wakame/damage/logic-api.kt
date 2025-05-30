a26f0e17cb5984c2202dc76347f6fb61ec55e6d1
__FlandreScarlet <1903925559@qq.com>
2025-05-30 20:48:28 +0800

    移除无用的 PreprocessDamageEvent

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/damage/logic-api.kt
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/damage/logic-api.kt
@@ -2,14 +2,8 @@
 
 package cc.mewcraft.wakame.damage
 
-import cc.mewcraft.wakame.entity.attribute.AttributeMapSnapshot
-import cc.mewcraft.wakame.entity.player.attributeContainer
-import cc.mewcraft.wakame.event.bukkit.PreprocessDamageEvent
-import cc.mewcraft.wakame.event.bukkit.PreprocessDamageEvent.Phase
 import org.bukkit.damage.DamageSource
-import org.bukkit.damage.DamageType
 import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 import org.jetbrains.annotations.ApiStatus
@@ -85,67 +79,5 @@ class DamageContext(
 ) {
     override fun toString(): String {
         return "DamageContext(damage=$damage, damagee=$damagee, damageCause=$damageCause, damageType=${damageSource.damageType}, causingEntity=${damageSource.causingEntity}, directEntity=${damageSource.directEntity}, damageLocation=${damageSource.damageLocation})"
-    }
-}
-
-/**
- * 用于方便构建 [PreprocessDamageEvent].
- */
-@ApiStatus.Internal
-@Deprecated("不采用基于事件的设计模式来支持各种逻辑的伤害修改")
-object PreprocessDamageEventFactory {
-
-    fun launchProjectile(causingEntity: Player): PreprocessDamageEvent {
-        return PreprocessDamageEvent(
-            phase = Phase.SEARCHING_TARGET,
-            causingEntity = causingEntity,
-            causingAttributes = causingEntity.attributeContainer.getSnapshot(),
-            _damageeEntity = null,
-            _damageSource = null
-        )
-    }
-
-    fun searchingTarget(causingEntity: Player): PreprocessDamageEvent {
-        return PreprocessDamageEvent(
-            phase = Phase.SEARCHING_TARGET,
-            causingEntity = causingEntity,
-            causingAttributes = causingEntity.attributeContainer.getSnapshot(),
-            _damageeEntity = null,
-            _damageSource = null
-        )
-    }
-
-    fun actuallyDamage(causingEntity: Player, damageeEntity: LivingEntity): PreprocessDamageEvent {
-        return PreprocessDamageEvent(
-            phase = Phase.ACTUALLY_DAMAGE,
-            causingEntity = causingEntity,
-            causingAttributes = causingEntity.attributeContainer.getSnapshot(),
-            _damageeEntity = damageeEntity,
-            _damageSource = DamageSource.builder(DamageType.GENERIC)
-                .withCausingEntity(causingEntity)
-                .withDirectEntity(causingEntity)
-                .withDamageLocation(damageeEntity.location)
-                .build(),
-        )
-    }
-
-    fun actuallyDamage(causingEntity: Player, damageContext: DamageContext): PreprocessDamageEvent {
-        return PreprocessDamageEvent(
-            phase = Phase.ACTUALLY_DAMAGE,
-            causingEntity = causingEntity,
-            causingAttributes = causingEntity.attributeContainer.getSnapshot(),
-            _damageeEntity = damageContext.damagee,
-            _damageSource = damageContext.damageSource,
-        )
-    }
-
-    fun actuallyDamage(causingEntity: Player, causingAttributes: AttributeMapSnapshot, damageContext: DamageContext): PreprocessDamageEvent {
-        return PreprocessDamageEvent(
-            phase = Phase.ACTUALLY_DAMAGE,
-            causingEntity = causingEntity,
-            causingAttributes = causingAttributes,
-            _damageeEntity = damageContext.damagee,
-            _damageSource = damageContext.damageSource,
-        )
     }
 }
