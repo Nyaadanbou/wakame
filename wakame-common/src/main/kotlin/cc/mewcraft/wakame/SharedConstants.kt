bae19fd68e16384b4a2f42ba8c5b4676094dcb21
g2213swo <2057581537@qq.com>
2025-06-27 16:46:27 +0800

    添加物品版本更新功能

--- wakame-common/src/main/kotlin/cc/mewcraft/wakame/SharedConstants.kt
+++ wakame-common/src/main/kotlin/cc/mewcraft/wakame/SharedConstants.kt
@@ -4,7 +4,8 @@ package cc.mewcraft.wakame
  * 适用于整个萌芽项目的常量.
  */
 object SharedConstants {
-    const val PLUGIN_NAME = "wakame"
+    const val PLUGIN_NAME = "koish"
+    const val DATA_VERSION = 1
 
     /**
      * 检查 JVM 是否在 IDE 中运行.
