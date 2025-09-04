3a316529be7377081f2d611010294fd2f21387bd
Nailm <porridgen@gmail.com>
2025-09-04 14:59:29 +0800

    修复合成配方 Mixin (#397)
    * 修复配方 Mixin
    
    * 修复编译错误
    
    * should be final
    
    * 整理 javadoc

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinStackedContentsExtrasMap.java
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinStackedContentsExtrasMap.java
@@ -1,31 +1,21 @@
 package cc.mewcraft.wakame.mixin.core;
 
-import cc.mewcraft.wakame.mixin.support.CustomItemStackLinkedSet;
+import cc.mewcraft.wakame.mixin.support.CustomItemStack;
 import io.papermc.paper.inventory.recipe.StackedContentsExtrasMap;
-import it.unimi.dsi.fastutil.Hash;
 import it.unimi.dsi.fastutil.objects.ObjectOpenCustomHashSet;
+import it.unimi.dsi.fastutil.objects.ObjectSet;
 import net.minecraft.world.item.ItemStack;
-import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = StackedContentsExtrasMap.class)
 public abstract class MixinStackedContentsExtrasMap {
 
     /**
-     * @author Nailm, Flandre
-     * @reason 让 Koish 物品在 Shapeless 配方中只考虑 Koish 的唯一物品标识而忽略其他物品数据
+     * 让 Koish 物品在 Shapeless 配方中只考虑 Koish 的唯一物品标识而忽略其他物品数据
      */
-    @Redirect(
-            method = "<init>",
-            at = @At(
-                    value = "NEW",
-                    target = "Lit/unimi/dsi/fastutil/objects/ObjectOpenCustomHashSet;",
-                    opcode = Opcodes.NEW
-            )
-    )
-    public ObjectOpenCustomHashSet<ItemStack> koish$ObjectOpenCustomHashSet(final Hash.Strategy<ItemStack> strategy) {
-        return new ObjectOpenCustomHashSet<>(CustomItemStackLinkedSet.KOISH_STRATEGY);
-    }
+    @Final
+    @Shadow
+    public final ObjectSet<ItemStack> exactIngredients = new ObjectOpenCustomHashSet<>(CustomItemStack.EXACT_MATCH_STRATEGY);
 }
---
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/support/CustomItemStack.java
@@ -0,0 +1,85 @@
+package cc.mewcraft.wakame.mixin.support;
+
+import cc.mewcraft.wakame.item2.KoishStackData;
+import it.unimi.dsi.fastutil.Hash;
+import net.kyori.adventure.key.Key;
+import net.minecraft.world.item.ItemStack;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
+
+/**
+ * 包含了让 Koish 物品堆叠在配方中只考虑物品类型(ID)的哈希策略和比较方法.
+ */
+public class CustomItemStack {
+
+    /**
+     * 用于比较 {@link org.bukkit.inventory.RecipeChoice} 中的物品堆叠的哈希策略.
+     * <p>
+     * 策略特点:
+     * 使得 Koish 物品堆叠在参与配方匹配时只考虑 Koish 物品类型(ID), 而忽略其他物品组件.
+     */
+    public static final Hash.Strategy<ItemStack> EXACT_MATCH_STRATEGY = new Hash.Strategy<>() {
+        @Override
+        public int hashCode(@Nullable ItemStack stack) {
+            return CustomItemStack.hashCode(stack);
+        }
+
+        @Override
+        public boolean equals(@Nullable ItemStack first, @Nullable ItemStack second) {
+            return CustomItemStack.equals(first, second);
+        }
+    };
+
+    /**
+     * 计算物品堆叠的哈希值.
+     * <p>
+     * 如果物品堆叠是 Koish 物品, 则哈希值基于 Koish 物品类型(ID);
+     * 如果物品堆叠不是 Koish 物品, 则哈希值基于 Minecraft 物品类型.
+     *
+     * @param stack 物品堆叠
+     * @return 物品的哈希值
+     */
+    public static int hashCode(@Nullable ItemStack stack) {
+        if (stack == null) return 0;
+        int h = 31 + stack.getItem().hashCode();
+        Key id = KoishStackData.getKoishTypeId(stack);
+        if (id != null) {
+            return 31 * h + id.hashCode();
+        } else {
+            // FIXME #396
+            // 目前的临时考虑: 对于 ExactChoice 中的非 Koish 物品, 只考虑 Minecraft 物品类型, 忽略其所有额外组件
+            // 这实际上破坏了 Bukkit API 中关于 ExactChoice 的定义, 但实际情况是, 只要没有其他插件注册使用了 ExactChoice 的配方, 那么就没有问题
+            // 我们需要考虑更好的实现, 即保证 ExactChoice 的语义不被破坏, 同时又能让 Koish 物品在配方中只考虑物品类型(ID)
+            return 31 * h;
+            //return 31 * h + stack.getComponents().hashCode();
+        }
+    }
+
+    /**
+     * 检查两个物品堆叠是否属于同一个 Koish 物品或具有相同的 Minecraft 物品类型.
+     *
+     * @param first  第一个物品堆叠
+     * @param second 第二个物品堆叠
+     * @return 如果两个物品堆叠属于同一个 Koish 物品类型则直接返回 true, 否则继续判断是否具有相同的 Minecraft 物品类型
+     */
+    public static boolean equals(@Nullable ItemStack first, @Nullable ItemStack second) {
+        // FIXME #396
+        return first == second || first != null && second != null && first.isEmpty() == second.isEmpty() && (isSameKoishItemType(first, second) || ItemStack.isSameItem(first, second));
+        //return first == second || first != null && second != null && first.isEmpty() == second.isEmpty() && (isSameKoishItemType(first, second) || ItemStack.isSameItemSameComponents(first, second));
+    }
+
+    /**
+     * 检查两个物品堆叠是否属于同一个 Koish 物品类型.
+     *
+     * @param first  第一个物品堆叠
+     * @param second 第二个物品堆叠
+     * @return 如果两个物品堆叠属于同一个 Koish 物品类型, 则返回 true; 否则返回 false
+     */
+    public static boolean isSameKoishItemType(@NonNull ItemStack first, @NonNull ItemStack second) {
+        Key id1 = KoishStackData.getKoishTypeId(first);
+        Key id2 = KoishStackData.getKoishTypeId(second);
+        return Objects.equals(id1, id2);
+    }
+}
