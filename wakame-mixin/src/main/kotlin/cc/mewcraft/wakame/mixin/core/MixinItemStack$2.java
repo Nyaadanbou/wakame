38da39863b7d2d5e17c88570a70be7e43fbef166
Ciallo <porridgen@gmail.com>
2025-09-06 10:08:19 +0800

    规范 Mixin 命名

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinItemStack$2.java
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinItemStack$2.java
@@ -1,5 +1,6 @@
 package cc.mewcraft.wakame.mixin.core;
 
+import cc.mewcraft.wakame.mixin.support.DataComponentPatchExtras;
 import cc.mewcraft.wakame.mixin.support.ExtraDataComponents;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.network.RegistryFriendlyByteBuf;
@@ -9,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "net.minecraft.world.item.ItemStack$2") // ItemStack#createOptionalStreamCodec
-public abstract class MixinItemStackOptionalStreamCodec {
+public abstract class MixinItemStack$2 {
 
     @Redirect(
             method = "encode(Lnet/minecraft/network/RegistryFriendlyByteBuf;Lnet/minecraft/world/item/ItemStack;)V",
@@ -22,7 +23,6 @@ public abstract class MixinItemStackOptionalStreamCodec {
     private void encode(StreamCodec<RegistryFriendlyByteBuf, DataComponentPatch> instance, Object buf, Object patch) {
         // 发包时不发送 Koish 添加的 DataComponent
         // 注: 其实可以改发包实现, 但 Mixin 性能更好
-        instance.encode((RegistryFriendlyByteBuf) buf, ((DataComponentPatch) patch).forget(ExtraDataComponents::isCustomType));
+        instance.encode((RegistryFriendlyByteBuf) buf, ((DataComponentPatchExtras) patch).koish$intrusiveRemove(ExtraDataComponents::isCustomType));
     }
-
 }
