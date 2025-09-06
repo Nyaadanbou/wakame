32b3510c2f16dff0866de4a52a3f2b4825ddde51
Ciallo <porridgen@gmail.com>
2025-09-06 16:40:02 +0800

    fix QuickShopHook

--- wakame-hooks/wakame-hook-quickshop/src/main/kotlin/cc/mewcraft/wakame/hook/impl/quickshop/QuickShopHook.kt
+++ wakame-hooks/wakame-hook-quickshop/src/main/kotlin/cc/mewcraft/wakame/hook/impl/quickshop/QuickShopHook.kt
@@ -1,24 +1,24 @@
 package cc.mewcraft.wakame.hook.impl.quickshop
 
 import cc.mewcraft.wakame.LOGGER
+import cc.mewcraft.wakame.SERVER
 import cc.mewcraft.wakame.adventure.translator.TranslatableMessages
 import cc.mewcraft.wakame.integration.Hook
 import cc.mewcraft.wakame.item2.config.property.ItemPropertyTypes
 import cc.mewcraft.wakame.item2.hasProperty
 import cc.mewcraft.wakame.item2.isExactKoish
-import cc.mewcraft.wakame.item2.isKoish
 import cc.mewcraft.wakame.item2.koishTypeId
 import cc.mewcraft.wakame.util.registerEvents
 import com.ghostchu.quickshop.api.event.QSCancellable
 import com.ghostchu.quickshop.api.event.display.ItemPreviewComponentPrePopulateEvent
 import com.ghostchu.quickshop.api.event.general.ShopItemMatchEvent
-import com.ghostchu.quickshop.api.event.management.ShopCreateEvent
 import com.ghostchu.quickshop.api.event.settings.type.ShopItemEvent
 import com.ghostchu.quickshop.api.event.settings.type.ShopTypeEvent
 import com.ghostchu.quickshop.api.shop.Shop
 import com.ghostchu.quickshop.api.shop.ShopType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.inventory.ItemStack
 
 @Hook(plugins = ["QuickShop-Hikari"])
 object QuickShopHook : Listener {
@@ -37,21 +37,16 @@ object QuickShopHook : Listener {
         }
     }
 
-    // 在玩家创建[收购类型]的商店时, 如果物品类型不允许收购, 则阻止商店创建
-    @EventHandler
-    fun on(event: ShopCreateEvent) {
-        LOGGER.info("$event")
-        if (event.phase().cancellable()) {
-            tryCancel(event.shop().get(), event)
-        }
-    }
-
     // 在玩家更新[收购类型]的商店时, 如果物品类型不允许收购, 则阻止商店更新
     @EventHandler
     fun on(event: ShopTypeEvent) {
         LOGGER.info("$event")
         if (event.phase().cancellable()) {
-            tryCancel(event.shop(), event)
+            if (event.updated() == ShopType.BUYING) {
+                val shop = event.shop()
+                val shopItem = shop.item
+                tryCancel(shop, shopItem, event)
+            }
         }
     }
 
@@ -60,7 +55,11 @@ object QuickShopHook : Listener {
     fun on(event: ShopItemEvent) {
         LOGGER.info("$event")
         if (event.phase().cancellable()) {
-            tryCancel(event.shop(), event)
+            val shop = event.shop()
+            if (shop.shopType == ShopType.BUYING) {
+                val shopItem = event.updated()
+                tryCancel(shop, shopItem, event)
+            }
         }
     }
 
@@ -69,11 +68,12 @@ object QuickShopHook : Listener {
         LOGGER.info("$event")
     }
 
-    private fun tryCancel(shop: Shop, event: QSCancellable) {
-        if (shop.shopType == ShopType.BUYING) {
-            val shopItem = shop.item
-            if (shopItem.isKoish && !shopItem.hasProperty(ItemPropertyTypes.PLAYER_PURCHASABLE)) {
-                event.setCancelled(true, TranslatableMessages.MSG_MSG_ITEM_NOT_PURCHASABLE.build())
+    private fun tryCancel(shop: Shop, shopItem: ItemStack, event: QSCancellable) {
+        if (shopItem.isExactKoish && !shopItem.hasProperty(ItemPropertyTypes.PLAYER_PURCHASABLE)) {
+            event.setCancelled(true, TranslatableMessages.MSG_ITEM_NOT_PURCHASABLE.build())
+            shop.permissionAudiences.keys.mapNotNull { SERVER.getPlayer(it) }.forEach { player ->
+                // 通知所有在线的商店管理者
+                player.sendMessage(TranslatableMessages.MSG_ITEM_NOT_PURCHASABLE.build())
             }
         }
     }
