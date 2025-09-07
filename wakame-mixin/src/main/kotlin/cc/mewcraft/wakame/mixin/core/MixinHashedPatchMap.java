f4888328adb1ce82b6dcf2008e27184358cf796b
Ciallo <porridgen@gmail.com>
2025-09-07 22:46:10 +0800

    之前的方案达不到预期效果, 这个方案更好

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinHashedPatchMap.java
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinHashedPatchMap.java
@@ -2,38 +2,64 @@ package cc.mewcraft.wakame.mixin.core;
 
 import cc.mewcraft.wakame.mixin.support.ExtraDataComponents;
 import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.HashedPatchMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.Set;
 
 @Mixin(HashedPatchMap.class)
 public class MixinHashedPatchMap {
 
     /**
-     * 使得服务端在计算物品哈希时, 忽略掉 `koish:data_container` 物品组件.
+     * 让 {@link HashedPatchMap} 不考虑 `minecraft:lore` 物品组件.
      *
-     * @param patch 服务端上的 {@link DataComponentPatch}
-     * @return 忽略掉 `koish:data_container` 物品组件后的 {@link DataComponentPatch}
+     * @param addedComponents   客户端侧添加的物品组件
+     * @param removedComponents 客户端侧移除的物品组件
      */
-    // 总结:
-    // 这里的 matches 原本的逻辑是计算服务端侧数据(DataComponentPatch)的哈希, 与客户端发过来的哈希进行比较.
-    // 如果哈希不一致, 那么 matches 函数则返回 false, 之后服务端会重新发送 Set Container Slot 以纠正客户端上的“错误数据”.
-    // 解决办法就是在 matches 函数一开始就先把服务端上的 `koish:data_container` 物品组件给移除掉,
-    // 这样剩下的服务端侧数据的哈希应该就与客户端发过来的是一致的了, 避免了服务端强行纠正.
-    @ModifyVariable(
-            method = "matches",
-            at = @At("HEAD"),
-            ordinal = 0,
-            argsOnly = true
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
     )
-    private DataComponentPatch modifyPatch(DataComponentPatch patch) {
-        var optional = patch.get(ExtraDataComponents.DATA_CONTAINER);
-        //noinspection OptionalAssignedToNull
-        if (optional != null) { // 这里 "不为 null" 的语义是存在 add 或 remove 该物品组件的数据
-            return patch.forget(ExtraDataComponents::isCustomType);
-        } else {
-            return patch;
-        }
+    private void injected0(
+            Map<DataComponentType<?>, Integer> addedComponents,
+            Set<DataComponentType<?>> removedComponents,
+            CallbackInfo ci
+    ) {
+        addedComponents.remove(DataComponents.LORE);
+        removedComponents.remove(DataComponents.LORE);
+    }
+
+    /**
+     * 使服务端在比较物品哈希时, 忽略 `minecraft:lore` 和 `koish:data_container` 物品组件.
+     *
+     * @param patch         服务端侧的 {@link DataComponentPatch}
+     * @param hashGenerator 哈希生成器
+     */
+    // 这里的 matches 原本逻辑:
+    //   计算服务端侧数据(DataComponentPatch)的哈希, 与客户端发过来的哈希进行比较.
+    //   如果哈希不一致, 那么 matches 函数则返回 false,
+    //   之后服务端会重新发送 Set Container Slot 以纠正客户端上的“错误数据”.
+    // 解决办法:
+    //   在 matches 函数一开始就先把服务端侧的 patch 是否包含 `koish:data_container` 物品组件,
+    //   如果包含 (即为 Koish 物品), 则直接返回 true (即假装哈希一致), 让服务端始终不纠正该数据.
+    // 潜在问题:
+    //   未知
+    @Inject(
+            method = "matches",
+            at = @At("HEAD")
+    )
+    private void injected1(
+            DataComponentPatch patch,
+            HashedPatchMap.HashGenerator hashGenerator,
+            CallbackInfoReturnable<Boolean> cir
+    ) {
+        patch.forget(t -> t == DataComponents.LORE || t == ExtraDataComponents.DATA_CONTAINER);
     }
 }
