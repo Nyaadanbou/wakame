d603f7a41f3a16708c9a13929c0eb20ac4adf0c0
Ciallo <porridgen@gmail.com>
2025-09-07 23:14:26 +0800

    更新文档

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinHashedPatchMap.java
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/mixin/core/MixinHashedPatchMap.java
@@ -43,11 +43,11 @@ public class MixinHashedPatchMap {
      */
     // 这里的 matches 原本逻辑:
     //   计算服务端侧数据(DataComponentPatch)的哈希, 与客户端发过来的哈希进行比较.
-    //   如果哈希不一致, 那么 matches 函数则返回 false,
-    //   之后服务端会重新发送 Set Container Slot 以纠正客户端上的“错误数据”.
+    //   如果哈希不一致, 那么 matches 函数则返回 false, 之后服务端会重新发送 Set Container Slot 以纠正客户端上的“错误数据”.
+    //   如果哈希一致, 则不进行纠正.
     // 解决办法:
-    //   在 matches 函数一开始就先把服务端侧的 patch 是否包含 `koish:data_container` 物品组件,
-    //   如果包含 (即为 Koish 物品), 则直接返回 true (即假装哈希一致), 让服务端始终不纠正该数据.
+    //   在 matches 函数一开始就先移除服务端侧 patch 中的 `minecraft:lore` 和 `koish:data_container` 物品组件,
+    //   然后在已经忽略客户端侧 `minecraft:lore` 组件哈希的前提下, 比较服务端侧和客户端侧的物品哈希.
     // 潜在问题:
     //   未知
     @ModifyVariable(
