db01b7dc437dc994b9d5802ebdba63523282e02e
Ciallo <porridgen@gmail.com>
2025-09-23 17:38:02 +0800

    支持禁止玩家在特定世界与村民交易

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/adventure/translator/TranslatableMessages.kt
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/adventure/translator/TranslatableMessages.kt
@@ -84,6 +84,7 @@ object TranslatableMessages {
     val MSG_HOLD_LAST_DAMAGE_DEFAULT_WHEN_CAUSE_DAMAGE = create("msg_hold_last_damage_default_when_cause_damage")
     val MSG_HOLD_LAST_DAMAGE_DEFAULT_WHEN_RECEIVE_DAMAGE = create("msg_hold_last_damage_default_when_receive_damage")
     val MSG_HOLD_LAST_DAMAGE_DEFAULT_WHEN_CONSUME = create("msg_hold_last_damage_default_when_consume")
+    val MSG_VILLAGER_TRADE_DISABLED_IN_THIS_WORLD = create("msg_villager_trade_disabled_in_this_world")
 
     private fun create(key: String): TranslatableComponent.Builder {
         return Component.translatable().key(key)
