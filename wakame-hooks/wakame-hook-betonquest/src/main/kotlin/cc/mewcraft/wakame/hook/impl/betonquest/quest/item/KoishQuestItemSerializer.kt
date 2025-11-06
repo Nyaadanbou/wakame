5bdede6cc96d666cb7d1f78d85d56bf005fd9490
Ciallo <porridgen@gmail.com>
2025-11-06 17:10:27 +0800

    不包含命名空间, 因为都是统一的 "koish"

--- wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItemSerializer.kt
+++ wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItemSerializer.kt
@@ -8,6 +8,6 @@ class KoishQuestItemSerializer : QuestItemSerializer {
 
     override fun serialize(itemstack: ItemStack): String {
         val typeId = itemstack.koishTypeId ?: throw IllegalArgumentException("ItemStack is not a exact Koish item")
-        return typeId.toString()
+        return typeId.value() // 不包含命名空间, 因为都是统一的 "koish"
     }
 }
