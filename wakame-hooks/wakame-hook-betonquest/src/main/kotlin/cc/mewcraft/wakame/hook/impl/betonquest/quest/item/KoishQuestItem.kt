a8053478a629442f05e27654183a44cc76aa11a6
Ciallo <porridgen@gmail.com>
2025-11-06 17:07:24 +0800

    不需要每次都生成一个 ItemStack

--- wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItem.kt
+++ wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItem.kt
@@ -1,9 +1,8 @@
 package cc.mewcraft.wakame.hook.impl.betonquest.quest.item
 
-import cc.mewcraft.wakame.item.KoishItem
-import cc.mewcraft.wakame.item.getProperty
-import cc.mewcraft.wakame.item.koishTypeId
-import cc.mewcraft.wakame.item.name
+import cc.mewcraft.wakame.integration.playerlevel.PlayerLevelIntegration
+import cc.mewcraft.wakame.item.*
+import cc.mewcraft.wakame.item.datagen.ItemGenerationContext
 import cc.mewcraft.wakame.item.property.ItemPropTypes
 import net.kyori.adventure.text.Component
 import org.betonquest.betonquest.api.profile.Profile
@@ -11,7 +10,6 @@ import org.betonquest.betonquest.item.QuestItem
 import org.bukkit.inventory.ItemStack
 
 class KoishQuestItem(
-    private val resolvedItem: ItemStack,
     private val itemType: KoishItem,
 ) : QuestItem {
 
@@ -24,7 +22,15 @@ class KoishQuestItem(
     }
 
     override fun generate(stackSize: Int, profile: Profile?): ItemStack {
-        return resolvedItem.clone()
+        val itemstack = if (profile == null) {
+            val ctx = ItemGenerationContext(itemType, 0f, 0)
+            KoishStackGenerator.generate(itemType, ctx)
+        } else {
+            val level = PlayerLevelIntegration.get(profile.playerUUID) ?: 0
+            val ctx = ItemGenerationContext(itemType, 0f, level)
+            KoishStackGenerator.generate(itemType, ctx)
+        }
+        return itemstack
     }
 
     override fun matches(item: ItemStack?): Boolean {
--- wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItemWrapper.kt
+++ wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/quest/item/KoishQuestItemWrapper.kt
@@ -1,9 +1,6 @@
 package cc.mewcraft.wakame.hook.impl.betonquest.quest.item
 
-import cc.mewcraft.wakame.integration.playerlevel.PlayerLevelIntegration
 import cc.mewcraft.wakame.item.KoishItem
-import cc.mewcraft.wakame.item.KoishStackGenerator
-import cc.mewcraft.wakame.item.datagen.ItemGenerationContext
 import org.betonquest.betonquest.api.instruction.variable.Variable
 import org.betonquest.betonquest.api.profile.Profile
 import org.betonquest.betonquest.item.QuestItem
@@ -15,14 +12,6 @@ class KoishQuestItemWrapper(
 
     override fun getItem(profile: Profile?): QuestItem {
         val itemType = itemType.getValue(profile)
-        val itemstack = if (profile == null) {
-            val ctx = ItemGenerationContext(itemType, 0f, 0)
-            KoishStackGenerator.generate(itemType, ctx)
-        } else {
-            val level = PlayerLevelIntegration.get(profile.playerUUID) ?: 0
-            val ctx = ItemGenerationContext(itemType, 0f, level)
-            KoishStackGenerator.generate(itemType, ctx)
-        }
-        return KoishQuestItem(itemstack, itemType)
+        return KoishQuestItem(itemType)
     }
 }
