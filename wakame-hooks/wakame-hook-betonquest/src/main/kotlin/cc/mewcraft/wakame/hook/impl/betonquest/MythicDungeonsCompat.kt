55b283436768a8e7da6fbb8d50f6db5f13756caa
Ciallo <porridgen@gmail.com>
2025-11-11 15:09:53 +0800

    更稳定的实现

--- wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/MythicDungeonsCompat.kt
+++ wakame-hooks/wakame-hook-betonquest/src/main/kotlin/cc/mewcraft/wakame/hook/impl/betonquest/MythicDungeonsCompat.kt
@@ -1,7 +1,9 @@
 package cc.mewcraft.wakame.hook.impl.betonquest
 
+import cc.mewcraft.wakame.hook.impl.betonquest.dungeon.MythicDungeonsListener
 import cc.mewcraft.wakame.hook.impl.betonquest.quest.event.EnterDungeonEventFactory
 import cc.mewcraft.wakame.integration.Hook
+import cc.mewcraft.wakame.util.registerEvents
 import org.betonquest.betonquest.BetonQuest
 
 @Hook(plugins = ["BetonQuest", "MythicDungeons"])
@@ -13,5 +15,9 @@ object MythicDungeonsCompat {
         val questRegistries = BetonQuest.getInstance().questRegistries
 
         questRegistries.event().register("playdungeon", EnterDungeonEventFactory(loggerFactory, primaryServerThreadData))
+    }
+
+    init {
+        MythicDungeonsListener.registerEvents()
     }
 }
