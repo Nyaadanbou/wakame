8f4d42a8cf59c63413844de8fd0c40332d62cc1e
Ciallo <porridgen@gmail.com>
2025-11-30 19:40:54 +0800

    新钩子: wakame-hook-plotsquared

---
+++ wakame-hooks/wakame-hook-plotsquared/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("koish-conventions.kotlin")
+    id("cc.mewcraft.libraries-repository")
+}
+
+version = "0.0.1"
+
+repositories {
+    nyaadanbouReleases()
+    nyaadanbouPrivate()
+    maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
+    maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
+    maven { url = uri("https://maven.enginehub.org/repo/") }
+}
+
+dependencies {
+    // internal
+    compileOnly(project(":wakame-mixin"))
+    compileOnly(project(":wakame-plugin"))
+
+    // libraries
+    compileOnly(local.paper)
+    implementation(platform("com.intellectualsites.bom:bom-newest:1.47"))
+    compileOnly("com.intellectualsites.plotsquared:plotsquared-core")
+    compileOnly("com.intellectualsites.plotsquared:plotsquared-bukkit") { isTransitive = false }
+    compileOnly(local.worldedit)
+}
