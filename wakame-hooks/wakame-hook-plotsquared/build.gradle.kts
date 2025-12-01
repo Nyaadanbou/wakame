ca39a8a9736949a4a3e5bc3ba2295ef4f26c0eea
Ciallo <porridgen@gmail.com>
2025-12-01 18:32:41 +0800

    更新 build.gradle.kts

--- wakame-hooks/wakame-hook-plotsquared/build.gradle.kts
+++ wakame-hooks/wakame-hook-plotsquared/build.gradle.kts
@@ -8,8 +8,12 @@ version = "0.0.1"
 repositories {
     nyaadanbouReleases()
     nyaadanbouPrivate()
+
+    // plugin: PlotSquared
     maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
+
+    // plugin: WorldEdit
     maven { url = uri("https://maven.enginehub.org/repo/") }
 }
 
@@ -19,9 +23,12 @@ dependencies {
     compileOnly(project(":wakame-plugin"))
 
     // libraries
-    compileOnly(local.paper)
+
+    // plugin: PlotSquared
     implementation(platform("com.intellectualsites.bom:bom-newest:1.47"))
     compileOnly("com.intellectualsites.plotsquared:plotsquared-core")
     compileOnly("com.intellectualsites.plotsquared:plotsquared-bukkit") { isTransitive = false }
+
+    // plugin: WorldEdit
     compileOnly(local.worldedit)
 }
