469e0a135bd93deb0bbfa10fae58607c93be5e56
Ciallo <porridgen@gmail.com>
2025-12-07 19:35:23 +0800

    实现 LuckPermsServerInfoProvider.kt

---
+++ wakame-hooks/wakame-hook-luckperms/src/main/kotlin/cc/mewcraft/wakame/hook/impl/luckperms/LuckPermsServerInfoProvider.kt
@@ -0,0 +1,36 @@
+package cc.mewcraft.wakame.hook.impl.luckperms
+
+import cc.mewcraft.wakame.messaging.ServerInfoProvider
+import net.luckperms.api.LuckPerms
+import net.luckperms.api.LuckPermsProvider
+import java.util.*
+import kotlin.jvm.optionals.getOrDefault
+
+/**
+ * 该实现假设 `LuckPerms/contexts.json` 中存在以下类似的定义:
+ *
+ * ```json
+ * {
+ *   "static-contexts": {
+ *     "server-id": "xhome1",
+ *     "server-group": "xhome",
+ *     "server-name": "家园"
+ *   },
+ *   "default-contexts": {}
+ * }
+ * ```
+ */
+internal class LuckPermsServerInfoProvider : ServerInfoProvider {
+
+    private val luckPermsApi: LuckPerms
+        get() = LuckPermsProvider.get()
+
+    override val serverId: UUID = UUID.randomUUID()
+
+    override val serverMemorableId: String
+        get() = luckPermsApi.contextManager.staticContext.getAnyValue("server-id").getOrDefault("unset")
+    override val serverGroupId: String
+        get() = luckPermsApi.contextManager.staticContext.getAnyValue("server-group").getOrDefault("unset")
+    override val serverName: String
+        get() = luckPermsApi.contextManager.staticContext.getAnyValue("server-name").getOrDefault("unset")
+}
