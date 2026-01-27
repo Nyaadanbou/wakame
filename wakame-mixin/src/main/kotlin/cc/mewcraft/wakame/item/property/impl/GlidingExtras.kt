9dbfa8368f0cd610edd945ef9c05210f7f88cea9
Ciallo <porridgen@gmail.com>
2026-01-27 11:17:37 +0800

    调整滑翔相关的默认魔法消耗为零

--- wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/item/property/impl/GlidingExtras.kt
+++ wakame-mixin/src/main/kotlin/cc/mewcraft/wakame/item/property/impl/GlidingExtras.kt
@@ -4,7 +4,7 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @ConfigSerializable
 data class GlidingExtras(
-    val glideDrainPerSecond: Double = 1.0,
-    val enterGlideManaCost: Double = 2.0,
-    val rocketBoostManaCost: Double = 3.0,
+    val glideDrainPerSecond: Double = 0.0,
+    val enterGlideManaCost: Double = 0.0,
+    val rocketBoostManaCost: Double = 0.0,
 )
