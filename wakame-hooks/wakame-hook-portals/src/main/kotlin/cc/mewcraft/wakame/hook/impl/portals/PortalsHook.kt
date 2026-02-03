bb356d8aefde4c7379edcaccd444af2113d7f38c
Ciallo <porridgen@gmail.com>
2026-02-03 15:08:05 +0800

    注册新的 Portals ActionType 并实现 OpenMenuAction

--- wakame-hooks/wakame-hook-portals/src/main/kotlin/cc/mewcraft/wakame/hook/impl/portals/PortalsHook.kt
+++ wakame-hooks/wakame-hook-portals/src/main/kotlin/cc/mewcraft/wakame/hook/impl/portals/PortalsHook.kt
@@ -2,6 +2,8 @@ package cc.mewcraft.wakame.hook.impl.portals
 
 import cc.mewcraft.wakame.integration.Hook
 import cc.mewcraft.wakame.integration.teleport.RandomTeleport
+import net.thenextlvl.portals.action.ActionType
+import net.thenextlvl.portals.action.ActionTypeRegistry
 
 @Hook(plugins = ["Portals"])
 object PortalsHook {
@@ -9,5 +11,7 @@ object PortalsHook {
     init {
         // 注册 Portals 提供的 RandomTeleport 实现
         RandomTeleport.setImplementation(PortalsRandomTeleport())
+        // 注册 Portals 的新 ActionType
+        ActionTypeRegistry.registry().register(ActionType.create("open_menu", String::class.java, OpenMenuAction()))
     }
 }
