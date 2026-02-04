eba5fb70e1dc1472cf7325722d1d702f4115ab2f
Ciallo <porridgen@gmail.com>
2026-02-04 14:13:36 +0800

    清理 LoggerDecorator

--- wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/util/LoggerDecorator.kt
+++ wakame-plugin/src/main/kotlin/cc/mewcraft/wakame/util/LoggerDecorator.kt
@@ -1,12 +1,21 @@
 package cc.mewcraft.wakame.util
 
 import org.slf4j.Logger
+import org.slf4j.event.Level
+import org.slf4j.spi.LoggingEventBuilder
+import kotlin.reflect.KClass
 
 fun Logger.decorate(prefix: String): Logger =
     LoggerDecorator(this, prefix)
 
+fun Logger.decorate(klass: KClass<*>): Logger =
+    decorate("[${klass.simpleName}] ")
+
 fun Lazy<Logger>.decorate(prefix: String): Lazy<Logger> =
     map { value.decorate(prefix) }
+
+fun Lazy<Logger>.decorate(klass: KClass<*>): Lazy<Logger> =
+    map { value.decorate(klass) }
 
 /**
  * 为 [Logger] 提供装饰器功能.
@@ -14,25 +23,41 @@ fun Lazy<Logger>.decorate(prefix: String): Lazy<Logger> =
  * @param logger 要装饰的 [Logger]
  * @param prefix 装饰的前缀
  */
-class LoggerDecorator(
+private class LoggerDecorator(
     private val logger: Logger,
     private val prefix: String,
 ) : Logger by logger {
-    override fun debug(msg: String) = logger.debug("$prefix$msg")
-    override fun info(msg: String) = logger.info("$prefix$msg")
-    override fun warn(msg: String) = logger.warn("$prefix$msg")
-    override fun error(msg: String) = logger.error("$prefix$msg")
+    override fun makeLoggingEventBuilder(level: Level): LoggingEventBuilder = super.makeLoggingEventBuilder(level)
+    override fun atLevel(level: Level): LoggingEventBuilder = super.atLevel(level)
+    override fun isEnabledForLevel(level: Level): Boolean = super.isEnabledForLevel(level)
+
+    // TRACE
     override fun trace(msg: String) = logger.trace("$prefix$msg")
-
-    override fun debug(format: String, arg: Any) = logger.debug("$prefix$format", arg)
-    override fun info(format: String, arg: Any) = logger.info("$prefix$format", arg)
-    override fun warn(format: String, arg: Any) = logger.warn("$prefix$format", arg)
-    override fun error(format: String, arg: Any) = logger.error("$prefix$format", arg)
     override fun trace(format: String, arg: Any) = logger.trace("$prefix$format", arg)
-
-    override fun debug(format: String, vararg arguments: Any) = logger.debug("$prefix$format", *arguments)
-    override fun info(format: String, vararg arguments: Any) = logger.info("$prefix$format", *arguments)
-    override fun warn(format: String, vararg arguments: Any) = logger.warn("$prefix$format", *arguments)
-    override fun error(format: String, vararg arguments: Any) = logger.error("$prefix$format", *arguments)
     override fun trace(format: String, vararg arguments: Any) = logger.trace("$prefix$format", *arguments)
+    override fun atTrace(): LoggingEventBuilder = super.atTrace()
+
+    // DEBUG
+    override fun debug(msg: String) = logger.debug("$prefix$msg")
+    override fun debug(format: String, arg: Any) = logger.debug("$prefix$format", arg)
+    override fun debug(format: String, vararg arguments: Any) = logger.debug("$prefix$format", *arguments)
+    override fun atDebug(): LoggingEventBuilder = super.atDebug()
+
+    // INFO
+    override fun info(msg: String) = logger.info("$prefix$msg")
+    override fun info(format: String, arg: Any) = logger.info("$prefix$format", arg)
+    override fun info(format: String, vararg arguments: Any) = logger.info("$prefix$format", *arguments)
+    override fun atInfo(): LoggingEventBuilder = super.atInfo()
+
+    // WARN
+    override fun warn(msg: String) = logger.warn("$prefix$msg")
+    override fun warn(format: String, arg: Any) = logger.warn("$prefix$format", arg)
+    override fun warn(format: String, vararg arguments: Any) = logger.warn("$prefix$format", *arguments)
+    override fun atWarn(): LoggingEventBuilder = super.atWarn()
+
+    // ERROR
+    override fun error(msg: String) = logger.error("$prefix$msg")
+    override fun error(format: String, arg: Any) = logger.error("$prefix$format", arg)
+    override fun error(format: String, vararg arguments: Any) = logger.error("$prefix$format", *arguments)
+    override fun atError(): LoggingEventBuilder = super.atError()
 }
