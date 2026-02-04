5fa41781bdfda2871bae84bc56ba67dc4a3c4da3
Ciallo <porridgen@gmail.com>
2026-02-04 21:46:55 +0800

    Koish 重载时也重载 HibiscusCommons

--- wakame-hooks/wakame-hook-hibiscuscommons/src/main/kotlin/cc/mewcraft/wakame/hook/impl/hibiscuscommons/KoishHook.kt
+++ wakame-hooks/wakame-hook-hibiscuscommons/src/main/kotlin/cc/mewcraft/wakame/hook/impl/hibiscuscommons/KoishHook.kt
@@ -1,10 +1,13 @@
 package cc.mewcraft.wakame.hook.impl.hibiscuscommons
 
+import cc.mewcraft.wakame.api.event.KoishLoadDataEvent
 import cc.mewcraft.wakame.item.KoishItemRefHandler
 import cc.mewcraft.wakame.util.KOISH_NAMESPACE
+import me.lojosho.hibiscuscommons.api.events.HibiscusHookReload
 import me.lojosho.hibiscuscommons.hooks.Hook
 import me.lojosho.hibiscuscommons.hooks.HookFlag
 import net.kyori.adventure.key.Key
+import org.bukkit.event.EventHandler
 import org.bukkit.inventory.ItemStack
 
 class KoishHook : Hook(
@@ -22,5 +25,10 @@ class KoishHook : Hook(
 
     override fun load() {
         this.isActive = true // Koish 物品在 bootstrap 阶段就已经确定, 所以 isActive 可以始终返回 true
+    }
+
+    @EventHandler
+    fun on(event: KoishLoadDataEvent) {
+        HibiscusHookReload(this, HibiscusHookReload.ReloadType.RELOAD).callEvent()
     }
 }
