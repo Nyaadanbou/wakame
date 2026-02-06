fe5f111229bdaf8e3c48a15ad4b866e164cebd14
Ciallo <porridgen@gmail.com>
2026-02-06 17:46:46 +0800

    BetterGUI 支持直接使用 Koish 物品

--- standalone/bettergui-addon/koish-bridge/src/main/kotlin/cc/mewcraft/bettergui/KoishBridge.kt
+++ standalone/bettergui-addon/koish-bridge/src/main/kotlin/cc/mewcraft/bettergui/KoishBridge.kt
@@ -3,9 +3,11 @@ package cc.mewcraft.bettergui
 import cc.mewcraft.bettergui.action.GiveBalance
 import cc.mewcraft.bettergui.action.OpenCatalog
 import cc.mewcraft.bettergui.action.RequestTeleportOnJoin
+import cc.mewcraft.bettergui.modifier.KoishItemModifier
 import cc.mewcraft.bettergui.requirement.AccBalanceReq
 import cc.mewcraft.bettergui.requirement.BalanceReq
 import me.hsgamer.bettergui.builder.ActionBuilder
+import me.hsgamer.bettergui.builder.ItemModifierBuilder
 import me.hsgamer.bettergui.builder.RequirementBuilder
 import me.hsgamer.hscore.common.Validate
 import me.hsgamer.hscore.expansion.common.Expansion
@@ -20,6 +22,7 @@ class KoishBridge : Expansion {
         ActionBuilder.INSTANCE.register(::GiveBalance, "give-balance")
         ActionBuilder.INSTANCE.register(::OpenCatalog, "open-catalog")
         ActionBuilder.INSTANCE.register(::RequestTeleportOnJoin, "request-teleport-on-join")
+        ItemModifierBuilder.INSTANCE.register(::KoishItemModifier, "koish")
         RequirementBuilder.INSTANCE.register(::BalanceReq, "balance")
         RequirementBuilder.INSTANCE.register(::AccBalanceReq, "acc-balance")
     }
---
+++ standalone/bettergui-addon/koish-bridge/src/main/kotlin/cc/mewcraft/bettergui/modifier/KoishItemModifier.kt
@@ -0,0 +1,28 @@
+package cc.mewcraft.bettergui.modifier
+
+import cc.mewcraft.wakame.item.KoishItemRefHandler
+import cc.mewcraft.wakame.util.Identifiers
+import me.hsgamer.hscore.common.StringReplacer
+import me.hsgamer.hscore.minecraft.item.ItemModifier
+import org.bukkit.inventory.ItemStack
+import java.util.*
+
+class KoishItemModifier : ItemModifier<ItemStack> {
+    private lateinit var itemId: String
+
+    override fun modify(original: ItemStack, uuid: UUID?, stringReplacer: StringReplacer): ItemStack {
+        if (!::itemId.isInitialized) return original
+        val itemId2 = stringReplacer.replaceOrOriginal(itemId, uuid)
+        val identifier = Identifiers.tryParse(itemId2) ?: return original
+        val itemstack = KoishItemRefHandler.createItemStack(identifier, 1, null) ?: return original
+        return itemstack
+    }
+
+    override fun toObject(): Any? {
+        return if (::itemId.isInitialized) itemId else null
+    }
+
+    override fun loadFromObject(`object`: Any?) {
+        this.itemId = `object`.toString()
+    }
+}
