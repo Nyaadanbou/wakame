8d1db01f858271b7ec1aad8ad0c472e072c6893b
Ciallo <porridgen@gmail.com>
2026-02-28 23:49:11 +0800

    why we need to /mm reload for placeholders to work?

--- wakame-hooks/wakame-hook-mythicmobs/src/main/kotlin/cc/mewcraft/wakame/hook/impl/mythicmobs/listener/ReloadListener.kt
+++ wakame-hooks/wakame-hook-mythicmobs/src/main/kotlin/cc/mewcraft/wakame/hook/impl/mythicmobs/listener/ReloadListener.kt
@@ -1,8 +1,10 @@
 package cc.mewcraft.wakame.hook.impl.mythicmobs.listener
 
+import cc.mewcraft.wakame.LOGGER
 import cc.mewcraft.wakame.hook.impl.mythicmobs.placeholder.KoishPlaceholders
 import cc.mewcraft.wakame.mixin.support.EntityTypeWrapperObjects
 import io.lumine.mythic.bukkit.events.MythicReloadCompleteEvent
+import io.lumine.mythic.bukkit.events.MythicReloadEvent
 import io.lumine.mythic.bukkit.events.MythicReloadedEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -10,12 +12,22 @@ import org.bukkit.event.Listener
 object ReloadListener : Listener {
 
     @EventHandler
+    fun on(event: MythicReloadEvent) {
+        KoishPlaceholders.register(event.instance)
+        LOGGER.info("Registered KoishPlaceholders for MythicMobs")
+    }
+
+    @EventHandler
     fun on(event: MythicReloadedEvent) {
         KoishPlaceholders.register(event.instance)
+        LOGGER.info("Registered KoishPlaceholders for MythicMobs")
     }
 
     @EventHandler
     fun on(event: MythicReloadCompleteEvent) {
+        KoishPlaceholders.register(event.instance)
+        LOGGER.info("Registered KoishPlaceholders for MythicMobs")
         EntityTypeWrapperObjects.resetDelegates()
+        LOGGER.info("Reset EntityTypeWrapper delegates for datapacks")
     }
 }
